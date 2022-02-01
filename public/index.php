e35687c5b6594d1b0febd012abc740bd153c70bf
Julia Koroleva <youlka2@gmail.com>
2022-02-01 20:59:15 +0300

    added homestead

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>OTUS 2022</title>
+</head>
+<body>
+<h1>Курс PHP Professional</h1>
+<h2>Студент: Королева Юлия</h2>
+</body>
+</html>
