fa70a4bd410314821ad71fad5ccb9a76c2902246
Julia Koroleva <youlka2@gmail.com>
2022-02-02 09:26:08 +0300

    added docker minimum conf

--- public/index.php
+++ public/index.php
@@ -5,10 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>OTUS 2022</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Королева Юлия</title>
 </head>
 <body>
-<h1>Курс PHP Professional</h1>
-<h2>Студент: Королева Юлия</h2>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">PHP PROF 2022</a>
+</nav>
+<div class="container text-center">
+    <h1>Курс PHP Professional</h1>
+    <h2>Студент: Королева Юлия</h2>
+</div>
 </body>
 </html>
