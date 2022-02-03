bf15546d314e6f7e5ea4c31e57aa92748ed0fb04
decole <decole@rambler.ru>
2022-02-03 09:38:44 +0300

    clear fix

--- public/index.php
+++ public/index.php
@@ -9,11 +9,11 @@ if ($memcachedResponse == 1) {
     echo 'memcached is work! <br>' ;
 }
 
-$single_server = array(
+$single_server = [
     'host' => 'redis',
     'port' => 6379,
     'database' => 0,
-);
+];
 
 $redis = new \Predis\Client($single_server);
 $redis->set('library', 'predis');
@@ -34,3 +34,10 @@ if (!$mysql->connect_error) {
 } else {
     die("Connection failed: " . $mysql->connect_error);
 }
+
+?>
+
+<br>
+<h1>Курс PhP Professional</h1>
+<br>
+<h3>Галочкин Сергей</h3>
