495d2e7db21f2eb05d19098f72f21503154bae87
Curtis Jackson <kos28032@gmail.com>
2022-02-05 20:57:09 +0600

    Show installed versions of the modules

--- code/index.php
+++ code/index.php
@@ -1,3 +1,54 @@
 <?php
 
-echo 'Hello OTUS community!';
+/*************** General **********************/
+
+echo '<pre>';
+echo 'Hello OTUS community!' . PHP_EOL . PHP_EOL;
+echo sprintf('Installed version of the PHP: %s' . PHP_EOL . PHP_EOL, phpversion());
+
+/*************** Memcached **********************/
+
+try {
+    $memcached = new Memcached();
+    $memcached->addServer('memcached', 11211);
+    $version = $memcached->getVersion();
+    $version = reset($version);
+
+    echo sprintf('Installed version of the Memcached: %s' . PHP_EOL . PHP_EOL, $version);;
+} catch (Exception $exception) {
+    echo sprintf(
+        'Error. The Memcached has not been started with message: %s' . PHP_EOL . PHP_EOL,
+        $exception->getMessage()
+    );
+}
+
+/*************** Redis **********************/
+
+try {
+    $redis = new Redis();
+    $redis->connect('redis', 6379);
+
+    echo sprintf('Installed version of the Redis: %s'  . PHP_EOL . PHP_EOL, $redis->info()['redis_version']);
+} catch (Exception $exception) {
+    echo sprintf(
+        'Error. Redis has not been started with message: %s'  . PHP_EOL . PHP_EOL,
+        $exception->getMessage()
+    );
+}
+
+/*************** MySQL **********************/
+
+try {
+    $dbname   = $_ENV['MYSQL_DATABASE'];
+    $password = $_ENV['MYSQL_ROOT_PASSWORD'];
+
+    $mysql = new PDO("mysql:host=mysql;dbname={$dbname}", 'root', $password);
+    $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo sprintf(
+        'Installed version of the MySQL: %s' . PHP_EOL . PHP_EOL,
+        $mysql->getAttribute(PDO::ATTR_SERVER_VERSION)
+    );
+} catch(PDOException $exception) {
+    echo sprintf('Error. MySQL has not been started with message: %s', $exception->getMessage());
+}
