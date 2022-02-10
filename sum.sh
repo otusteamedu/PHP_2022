92173524ae4b168225b767f017e272e88a0ddf38
ilia <sidor1989@yandex.ru>
2022-02-10 23:45:23 +0300

    hw2 fix after check

--- sum.sh
+++ sum.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-re='^[0-9]+$'
+re='^[+-]?[0-9]+([.][0-9]+)?$'
 if ! [[ $1 =~ $re ]] ; then
     echo "error: Not a number" >&1; exit 1
 fi
 if ! [[ $2 =~ $re ]] ; then
     echo "error: Not a number" >&1; exit 1
 fi
-sum=$(( $1 + $2 )) 
-    echo "Sum is: $sum"
+awk "BEGIN{ print $1 + $2 }"
