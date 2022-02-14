a3efbe4e7727007a3daba322738455ae767c6ec4
mapaxa <alexeybespalov86@gmail.com>
2022-02-14 12:35:55 +0200

    #hw2
    - 3 tries of install added
    - bug fix

--- shell/sum.sh
+++ shell/sum.sh
@@ -3,6 +3,8 @@
 ARGS_QUANTITY=2
 regex='^[-+]?[0-9]+\.?[0-9]*$'
 BC_PKG="bc"
+COUNT_TRIES_TO_INSTALL_PKG=3
+
 
 if [[ $# -ne $ARGS_QUANTITY ]]
 then
@@ -19,22 +21,40 @@ fi
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $BC_PKG|grep "install ok installed")
 
 # if bc not installed
-if [ "" = "$PKG_OK" ]
-then
+fact_tries_of_install=0
+while [ "" = "$PKG_OK" ]
+do
+
   echo "No $BC_PKG package. This script requires package \"$BC_PKG\" to be installed"
   if read -p "Do yo want to install $BC_PKG package?" 
   then
   answer=$(echo $REPLY | tr '[:upper:]' '[:lower:]'])
 	  if [[ $answer = 'y' ]] || [[ $answer = 'yes' ]]
 	  then
-	  echo "Installing BC_PKG"
+	  echo "Installing $BC_PKG"
 	  sudo apt-get --yes install $BC_PKG
+	  PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $BC_PKG|grep "install ok installed")
 	  else 
-  	echo "Ok. bye. Script stopped."
+  	echo "Script stopped. Reason: $BC_PKG was not installed."
   	exit 1
 		fi
   fi
-fi
+  COUNT_TRIES_TO_INSTALL_PKG=$[ $COUNT_TRIES_TO_INSTALL_PKG - 1 ]
+  fact_tries_of_install=$[ $fact_tries_of_install + 1 ]
 
-sum=$(echo "$1+$2" | bc)
-echo "$1+$2=$sum"
+  if [ $COUNT_TRIES_TO_INSTALL_PKG -eq 0 ]
+  then
+      echo "$BC_PKG was not installed after $fact_tries_of_install tries. Try later."
+      exit 1
+  fi
+  /bin/sleep 1
+done
+
+
+if [ "install ok installed" = "$PKG_OK" ]
+then
+    sum=$(echo "$1+$2" | bc)
+    echo "$1+$2=$sum"
+else
+    echo "Script stopped. Reason: $BC_PKG was not installed."
+fi
