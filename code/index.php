02925c38d2495068d7b9ede6fec759e834ece123
masteritua <masteritua>
2022-02-21 18:49:17 +0200

    hw4

--- code/index.php
+++ code/index.php
@@ -1,5 +1,43 @@
 <?php
 
-echo "Привет, Otus!<br>".date("Y-m-d H:i:s");
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(400); exit;
+}
 
-phpinfo();
+function checkStringBrackets($string)
+{
+    $length = strlen($string);
+    $stack  = [];
+
+    for ($i = 0; $i < $length; $i++) {
+        switch ($string[$i]) {
+            case '(':
+                $stack[] = 0; break;
+            case ')':
+                if (array_pop($stack) !== 0) {
+                    return false;
+                }
+                break;
+            default: break;
+        }
+    }
+
+    return empty($stack);
+}
+
+$string = $_REQUEST['string'];
+
+try {
+
+    if (empty($string)) {
+        throw new \Exception();
+    }
+
+    if (checkStringBrackets($string) === false) {
+        throw new \Exception();
+    }
+
+} catch (\Exception $e) {
+    http_response_code(400); exit;
+}
+
