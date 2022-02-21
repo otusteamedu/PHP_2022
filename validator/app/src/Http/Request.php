ff5a03fe303ab9711da832b63946e25f41b3ff8d
Павел <p.gaponenko@worksolutions.ru>
2022-02-21 21:50:39 +0300

    PGaponenko/hw5. Исправлен вывод ошибки

--- validator/app/src/Http/Request.php
+++ validator/app/src/Http/Request.php
@@ -4,16 +4,13 @@ namespace Otus\App\Http;
 
 class Request
 {
-    private const PARAMETER_NOT_FOUND_ERROR = 'Ошибка: Параметр string не найден.';
-    private const BAD_REQUEST_CODE = 400;
-
     /**
      * @throws \Exception
      */
     public function post(string $key): string
     {
-        if (!array_key_exists('string', $_POST)) {
-            throw new \Exception(self::PARAMETER_NOT_FOUND_ERROR, self::BAD_REQUEST_CODE);
+        if (!array_key_exists($key, $_POST)) {
+            throw new \Exception(sprintf('Ошибка: Параметр %s не найден.', $key), 400);
         }
 
         return (string)$_POST[$key];
