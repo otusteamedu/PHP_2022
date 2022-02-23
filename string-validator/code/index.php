9e4b3f27d92f0f09f781bff7c5e8f9ff82a90d0e
Curtis Jackson <kos28032@gmail.com>
2022-02-23 20:13:56 +0600

    REFACTOR: Move html to view & add new class for responses

--- string-validator/code/index.php
+++ string-validator/code/index.php
@@ -5,9 +5,4 @@ use KonstantinDmitrienko\StringValidator\App\App;
 require 'vendor/autoload.php';
 
 $app = new App();
-
-if ($app->getPost()) {
-    $app->validateString();
-} else {
-    $app->showInputField();
-}
+$app->run();
--- string-validator/code/src/App/App.php
+++ string-validator/code/src/App/App.php
@@ -2,43 +2,33 @@
 
 namespace KonstantinDmitrienko\StringValidator\App;
 
+use KonstantinDmitrienko\StringValidator\Response\Response;
 use KonstantinDmitrienko\StringValidator\StringValidator\StringValidator;
 
 class App
 {
-    public function showInputField(): void
-    {
-        echo <<<HTML
-            <form action="" method="post">
-                <div style="display: flex;">
-                    <label for="string">Fill a string</label>
-                    <textarea id="string" name="string" cols="50" rows="5" style="margin-left: 10px;"></textarea>
-                </div>
-                <button type="submit">Validate</button>
-            </form>
-HTML;
+    protected string $string = '';
+    protected static string $viewDir = '';
+
+    public function __construct() {
+        self::$viewDir = $_SERVER['DOCUMENT_ROOT'] . '/view/';
     }
 
-    public function getPost(): bool
+    public function run(): void
     {
-        return isset($_POST['string']);
-    }
-
-    public function validateString(): void
-    {
-        $string = $_POST['string'];
-
-        if (empty($string)) {
-            http_response_code(400);
-            echo "Error: Required \"string\" parameter is empty.";
+        if (!isset($_POST['string'])) {
+            require self::$viewDir . 'components/form.php';
             return;
         }
 
-        http_response_code(200);
-        if (StringValidator::hasMatchedBrackets($string)) {
-            echo "Success: String is valid.";
+        $this->string = $_POST['string'];
+
+        if (!$this->string) {
+            Response::emptyPost();
+        } else if (StringValidator::hasMatchedBrackets($this->string)) {
+            Response::success();
         } else {
-            echo "Error: Your string is invalid.";
+            Response::failure();
         }
     }
 }
