1c4181f80733c131f375669e9d81e16af3a8aa60
Julia Koroleva <youlka2@gmail.com>
2022-02-28 16:00:54 +0300

    HW8 - fix schema

--- Cinema_postgres_create.sql
+++ Cinema_postgres_create.sql
@@ -1,66 +1,74 @@
 CREATE DATABASE "cinema";
 
-CREATE TABLE IF NOT EXISTS "cinema.cinema_hall" (
-	"id" serial NOT NULL,
+CREATE TABLE "cinema_hall" (
+	"hall_id" serial NOT NULL,
 	"name" varchar(100) NOT NULL,
-	CONSTRAINT "cinema_hall_pk" PRIMARY KEY ("id")
+	"quantity_of_places" integer NOT NULL,
+	CONSTRAINT "cinema_hall_pk" PRIMARY KEY ("hall_id")
 ) WITH (
   OIDS=FALSE
 );
 
-INSERT INTO "cinema.cinema_hall" ("id", "name") VALUES
-(1, "Hall 1"),
-(2, "Hall 2"),
-(3, "Hall 3"),
-(4, "Hall 4");
-
-
-CREATE TABLE IF NOT EXISTS "cinema.film" (
-	"id" serial NOT NULL,
+CREATE TABLE "film" (
+	"film_id" serial NOT NULL,
 	"title" varchar(255) NOT NULL,
 	"description" TEXT NOT NULL,
 	"poster" varchar(255) NOT NULL,
-	CONSTRAINT "film_pk" PRIMARY KEY ("id")
+	"premier_date" DATE NOT NULL,
+	CONSTRAINT "film_pk" PRIMARY KEY ("film_id")
 ) WITH (
   OIDS=FALSE
 );
 
+CREATE TABLE "ticket" (
+	"ticket_id" serial NOT NULL,
+	"price_id" integer NOT NULL,
+	"purchase_date" DATE NOT NULL,
+	CONSTRAINT "ticket_pk" PRIMARY KEY ("ticket_id")
+) WITH (
+  OIDS=FALSE
+);
 
-INSERT INTO "cinema.film" ("id", "title", "description", "poster") VALUES
-(1, "Film 1", "description 1", "poster_url 1"),
-(2, "Film 2", "description 2", "poster_url 2"),
-(3, "Film 3", "description 3", "poster_url 3"),
-(4, "Film 4", "description 4", "poster_url 4");
-
-
-CREATE TABLE IF NOT EXISTS "cinema.ticket" (
-	"id" serial NOT NULL,
-	"film_id" integer NOT NULL,
+CREATE TABLE "price" (
+	"price_id" serial NOT NULL,
 	"hall_id" integer NOT NULL,
-	"price" integer NOT NULL,
-	CONSTRAINT "ticket_pk" PRIMARY KEY ("id")
+	"place_type_id" integer NOT NULL,
+	"session_id" integer NOT NULL,
+	"film_id" integer NOT NULL,
+	"amount" integer NOT NULL,
+	CONSTRAINT "price_pk" PRIMARY KEY ("price_id")
 ) WITH (
   OIDS=FALSE
 );
 
+CREATE TABLE "place_type" (
+	"place_type_id" serial NOT NULL,
+	"type" varchar(255) NOT NULL,
+	CONSTRAINT "place_type_pk" PRIMARY KEY ("place_type_id")
+) WITH (
+  OIDS=FALSE
+);
 
-INSERT INTO "cinema.ticket" ("id", "film_id", "hall_id", "price") VALUES
-(1, 1, 1, 300),
-(2, 1, 1, 500),
-(3, 1, 1, 500),
-(4, 2, 4, 500),
-(5, 2, 4, 400),
-(6, 2, 4, 500),
-(7, 1, 1, 500),
-(8, 1, 1, 450),
-(9, 1, 1, 500),
-(10, 4, 2, 250);
+CREATE TABLE "session" (
+	"session_id" serial NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"time" TIME NOT NULL,
+	CONSTRAINT "session_pk" PRIMARY KEY ("session_id")
+) WITH (
+  OIDS=FALSE
+);
 
+ALTER TABLE "ticket" ADD CONSTRAINT "ticket_fk0" FOREIGN KEY ("price_id") REFERENCES "price"("price_id");
 
-ALTER TABLE "cinema.ticket" ADD CONSTRAINT "ticket_fk0" FOREIGN KEY ("film_id") REFERENCES "cinema.film"("id");
-ALTER TABLE "cinema.ticket" ADD CONSTRAINT "ticket_fk1" FOREIGN KEY ("hall_id") REFERENCES "cinema.cinema_hall"("id");
+ALTER TABLE "price" ADD CONSTRAINT "price_fk0" FOREIGN KEY ("hall_id") REFERENCES "cinema_hall"("hall_id");
+ALTER TABLE "price" ADD CONSTRAINT "price_fk1" FOREIGN KEY ("place_type_id") REFERENCES "place_type"("place_type_id");
+ALTER TABLE "price" ADD CONSTRAINT "price_fk2" FOREIGN KEY ("session_id") REFERENCES "session"("session_id");
+ALTER TABLE "price" ADD CONSTRAINT "price_fk3" FOREIGN KEY ("film_id") REFERENCES "film"("film_id");
 
 -- самый прибыльный фильм --
-SELECT MAX("t.price") as "profit", "f.film_name" FROM "cinema.ticket t"
-INNER JOIN "cinema.film f" ON "f.id" = "t.film_id"
-GROUP BY "t.price", "f.film_name"
+SELECT SUM("p.amount") as "profit", "f.film_name" FROM "ticket t"
+INNER JOIN "price p" ON "p.price_id" = "t.price_id"
+INNER JOIN "film f" ON "f.film_id" = "p.film_id"
+GROUP BY "profit", "f.film_name"
+ORDER BY "profit" DESC
+LIMIT 1;
