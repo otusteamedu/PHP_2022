5f401357a2568f46df1418b0bb05146d8f128df5
decole <decole@rambler.ru>
2022-03-04 09:57:45 +0300

    fix UML and add SQL file

--- import.sql
+++ import.sql
@@ -1,16 +1,27 @@
 CREATE DATABASE "cinema_busines";
 
+CREATE SEQUENCE sequence_id INCREMENT 1 START 1;
+
 CREATE TABLE "type_price"
 (
     "id" serial NOT NULL PRIMARY KEY,
     "name" varchar(250) NOT NULL,
     "description" varchar(2000),
-    "cost" integer NOT NULL,
+    "cost" DEC(10,2) NOT NULL,
 )
+
+CREATE TABLE price_history(
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "type_price_id" serial NOT NULL,
+    "date_time" TIMESTAMP NOT NULL,
+    "price" DEC(10,2) NOT NULL,
+);
+
+CREATE SEQUENCE serial START 1;
 
 CREATE TABLE "film"
 (
-    "uuid" serial NOT NULL PRIMARY KEY,
+    "id" serial NOT NULL PRIMARY KEY,
     "name" varchar(250) NOT NULL,
     "description" varchar(2000),
     "film_time" integer NOT NULL,
@@ -29,34 +40,37 @@ CREATE TABLE "cinema_hall"
 
 CREATE TABLE "cinema_sessions"
 (
-    "uuid" serial NOT NULL PRIMARY KEY,
-    "film_uuid" serial NOT NULL,
+    "id" serial NOT NULL PRIMARY KEY,
+    "film_id" serial NOT NULL,
     "date_time_start" TIMESTAMP NOT NULL,
     "cinema_hall_id" serial NOT NULL,
 )
 
 CREATE TABLE "cinema_hall_place"
 (
-    "uuid" serial NOT NULL PRIMARY KEY,
+    "id" serial NOT NULL PRIMARY KEY,
     "cinema_hall_id" serial NOT NULL,
-    "cinema_sessions_uuid" serial NOT NULL
+    "cinema_sessions_id" serial NOT NULL
 )
 
 CREATE TABLE "tickets"
 (
-    "uuid" serial NOT NULL PRIMARY KEY,
-    "cinema_session_uuid" serial NOT NULL,
+    "id" serial NOT NULL PRIMARY KEY,
+    "cinema_session_id" serial NOT NULL,
     "cinema_hall_id" serial NOT NULL,
     "cinema_hall_place_id" serial NOT NULL,
     "type_price_id" serial NOT NULL
 )
 
 ALTER TABLE "cinema_sessions" ADD CONSTRAINT "cinema_sessions_fk0" FOREIGN KEY ("cinema_hall_id") REFERENCES "cinema_hall"("id");
-ALTER TABLE "cinema_sessions" ADD CONSTRAINT "cinema_sessions_fk1" FOREIGN KEY ("film_uuid") REFERENCES "film"("uuid");
+ALTER TABLE "cinema_sessions" ADD CONSTRAINT "cinema_sessions_fk1" FOREIGN KEY ("film_id") REFERENCES "film"("id");
 
-ALTER TABLE "tickets" ADD CONSTRAINT "tickets_fk0" FOREIGN KEY ("cinema_session_uuid") REFERENCES "cinema_sessions"("uuid");
-ALTER TABLE "tickets" ADD CONSTRAINT "tickets_fk1" FOREIGN KEY ("cinema_hall_place_id") REFERENCES "cinema_hall_place"("uuid");
+ALTER TABLE "tickets" ADD CONSTRAINT "tickets_fk0" FOREIGN KEY ("cinema_session_id") REFERENCES "cinema_sessions"("id");
+ALTER TABLE "tickets" ADD CONSTRAINT "tickets_fk1" FOREIGN KEY ("cinema_hall_place_id") REFERENCES "cinema_hall_place"("id");
 ALTER TABLE "tickets" ADD CONSTRAINT "tickets_fk2" FOREIGN KEY ("type_price_id") REFERENCES "type_price"("id");
+ALTER TABLE "tickets" ADD CONSTRAINT "tickets_fk3" FOREIGN KEY ("price_history_id") REFERENCES "price_history"("id");
 
 ALTER TABLE "cinema_hall_place" ADD CONSTRAINT "cinema_hall_place_fk0" FOREIGN KEY ("cinema_hall_id") REFERENCES "cinema_hall"("id");
-ALTER TABLE "cinema_hall_place" ADD CONSTRAINT "cinema_hall_place_fk1" FOREIGN KEY ("cinema_session_uuid") REFERENCES "cinema_session"("uuid");
+ALTER TABLE "cinema_hall_place" ADD CONSTRAINT "cinema_hall_place_fk1" FOREIGN KEY ("cinema_session_id") REFERENCES "cinema_session"("id");
+
+ALTER TABLE "price_history" ADD CONSTRAINT "price_history_fk0" FOREIGN KEY ("type_price_id") REFERENCES "type_price"("id");
