d9b07c414abdee804499e1ce38d07b23b035ac34
Павел <p.gaponenko@worksolutions.ru>
2022-03-07 14:03:19 +0300

    PGaponenko/hw8. Изменена структура таблиц: добавлена отдельная таблица для хранения стоимости билетов

--- database/cinema.sql
+++ database/cinema.sql
@@ -9,10 +9,21 @@ CREATE TABLE film (
     timing TIME
 );
 
+CREATE TABLE sector (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE row (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    sector_id INT REFERENCES sector (id)
+);
+
 CREATE TABLE place (
     id SERIAL PRIMARY KEY,
     place_number INT,
-    row_number INT,
+    row_id INT REFERENCES row (id),
     hall_id INT REFERENCES hall (id)
 );
 
@@ -24,14 +35,43 @@ CREATE TABLE film_session (
     end_time TIMESTAMP
 );
 
+CREATE TABLE day_type (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE time_of_day (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE price (
+    id SERIAL PRIMARY KEY,
+    sector_id INT REFERENCES sector (id),
+    day_type_id INT REFERENCES day_type (id),
+    time_of_day_id INT REFERENCES time_of_day (id),
+    price MONEY
+);
+
 CREATE TABLE ticket (
     id SERIAL PRIMARY KEY,
-    price MONEY,
+    price_id INT REFERENCES price (id),
     film_session_id INT REFERENCES film_session (id),
     place_id INT REFERENCES place (id)
 );
 
 INSERT INTO hall (name) VALUES ('Зал 1'), ('Зал 2'), ('Зал 3'), ('Зал 4'), ('Зал 5');
+
+INSERT INTO sector (name) VALUES ('Сектор 1'), ('Сектор 2'), ('Сектор 3');
+
+INSERT INTO row (name, sector_id)
+VALUES
+       ('Ряд 1', 1),
+       ('Ряд 2', 1),
+       ('Ряд 3', 2),
+       ('Ряд 4', 2),
+       ('Ряд 5', 3),
+       ('Ряд 6', 3);
 
 INSERT INTO film (name, timing)
 VALUES
@@ -45,9 +85,9 @@ VALUES
 DO $$
     BEGIN
         FOR i IN 1..5 LOOP
-                FOR j IN 1..5 LOOP
+                FOR j IN 1..6 LOOP
                         FOR k IN 1..10 LOOP
-                                INSERT INTO place (place_number, row_number, hall_id) values(k, j, i);
+                                INSERT INTO place (place_number, row_id, hall_id) values(k, j, i);
                             END LOOP;
                     END LOOP;
             END LOOP;
@@ -84,28 +124,46 @@ VALUES
     (5, 6, '2022-03-02 16:00', '2022-03-02 17:55'),
     (5, 3, '2022-03-02 18:10', '2022-03-02 20:20');
 
-INSERT INTO ticket (price, film_session_id, place_id)
+INSERT INTO day_type (name) VALUES ('weekday'), ('weekends');
+
+INSERT INTO time_of_day (name) VALUES ('morning'), ('day'), ('evening');
+
+INSERT INTO price (sector_id, day_type_id, time_of_day_id, price)
 VALUES
-    (150, 1, 5), (150, 1, 22), (150, 1, 17),
-    (300, 2, 23), (300, 2, 43),
-    (150, 3, 21), (150, 3, 33), (250, 3, 13), (250, 3, 4), (250, 3, 19),
-    (250, 4, 43), (250, 4, 49),
-    (300, 5, 87), (300, 5, 94),
-    (400, 6, 67), (400, 6, 87), (400, 6, 56),
-    (200, 7, 71), (200, 7, 58), (200, 7, 82),
-    (200, 8, 66), (200, 8, 78), (200, 8, 95), (150, 8, 100), (150, 8, 74),
-    (300, 9, 67), (300, 9, 91), (300, 9, 75),
-    (200, 10, 101), (200, 10, 134), (200, 10, 112),
-    (250, 11, 141), (250, 11, 132), (250, 11, 104),
-    (200, 12, 109), (200, 12, 111),
-    (300, 13, 145), (300, 13, 139),
-    (300, 14, 119), (300, 14, 141),
-    (150, 15, 151), (150, 15, 176), (150, 15, 199),
-    (200, 16, 171), (200, 16, 186),
-    (250, 17, 156), (250, 17, 197),
-    (150, 18, 177), (150, 18, 188), (150, 18, 199),
-    (250, 19, 202), (250, 19, 245), (250, 19, 234), (200, 19, 204),
-    (150, 20, 210), (150, 20, 211),
-    (150, 21, 244), (150, 21, 245),
-    (150, 22, 215), (150, 22, 247),
-    (250, 23, 201), (250, 23, 202), (250, 23, 203);
+       (1, 1, 1, 100),
+       (1, 1, 2, 150),
+       (1, 1, 3, 200),
+       (2, 1, 1, 150),
+       (2, 1, 2, 200),
+       (2, 1, 3, 250),
+       (3, 1, 1, 100),
+       (3, 1, 2, 150),
+       (3, 1, 3, 200),
+       (1, 2, 1, 200),
+       (1, 2, 2, 250),
+       (1, 2, 3, 300),
+       (2, 2, 1, 300),
+       (2, 2, 2, 350),
+       (2, 2, 3, 400),
+       (3, 2, 1, 200),
+       (3, 2, 2, 250),
+       (3, 2, 3, 300);
+
+INSERT INTO ticket (price_id, film_session_id, place_id)
+VALUES
+    (1, 1, 5), (4, 1, 22), (1, 1, 17),
+    (5, 2, 23), (8, 2, 43),
+    (6, 6, 21), (6, 3, 33), (3, 3, 13),
+    (9, 4, 43), (9, 4, 49),
+    (4, 5, 87), (4, 5, 94),
+    (4, 6, 87), (7, 6, 56),
+    (8, 7, 58), (5, 7, 82),
+    (2, 8, 66), (2, 8, 78), (5, 8, 95),
+    (3, 9, 67), (6, 9, 91),
+    (7, 10, 112),
+    (5, 11, 141), (2, 11, 132),
+    (2, 12, 144), (5, 12, 139),
+    (6, 13, 145), (3, 13, 139),
+    (3, 14, 140), (6, 14, 141),
+    (4, 15, 220), (7, 15, 230),
+    (2, 16, 181), (2, 16, 186);
