eca9960e215c39cf03e02d4d48b9148bfa8249c5
Павел <p.gaponenko@worksolutions.ru>
2022-03-07 14:03:55 +0300

    PGaponenko/hw8. Исправлен запрос на получение самого прибыльного фильма

--- database/query.sql
+++ database/query.sql
@@ -1,8 +1,10 @@
-SELECT film.name, sum(ticket.price) as price
+SELECT film.name, sum(price.price) as price
 FROM film,
      ticket,
-     film_session
+     film_session,
+     price
 WHERE ticket.film_session_id = film_session.id
   AND film_session.film_id = film.id
+    AND ticket.price_id = price.id
 GROUP BY film.name
 ORDER BY price DESC LIMIT 1
