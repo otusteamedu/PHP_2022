b4037d3a0da288b6fc08fd0664f99166e637b0b9
Павел <p.gaponenko@worksolutions.ru>
2022-03-08 11:54:33 +0300

    PGaponenko/hw8. Изменено поле цены для билета

--- database/query.sql
+++ database/query.sql
@@ -1,10 +1,9 @@
-SELECT film.name, sum(price.price) as price
+SELECT film.name, sum(ticket.price) as price
 FROM film,
      ticket,
      film_session,
      price
 WHERE ticket.film_session_id = film_session.id
   AND film_session.film_id = film.id
-    AND ticket.price_id = price.id
 GROUP BY film.name
 ORDER BY price DESC LIMIT 1
