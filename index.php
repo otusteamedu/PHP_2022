d48bdbd26eb314b095f4139fe0657698615caaa5
viktor <gmac.net.ru@gmail.com>
2022-03-24 17:01:07 +0300

    Правки по кодревью

--- index.php
+++ index.php
@@ -8,10 +8,5 @@ $fileManager = new App\FileManager\FileManager();
 
 $inputName = trim($argv[1]);
 
-if (!$inputName) {
-    throw new \Exception('fill file name');
-}
-
 $validator = ValidatorFactory::createByEmail();
-
 $validator->validate($inputName);
