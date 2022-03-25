dc268fd13d28b9f8d7af2d5b6330233e8099b1ea
viktor <gmac.net.ru@gmail.com>
2022-03-25 13:07:07 +0300

    Правки по кодревью

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ use App\Validator\ValidatorFactory;
 
 require_once './vendor/autoload.php';
 
-$fileManager = new App\FileManager\FileManager();
 
 $validator = ValidatorFactory::createByEmail();
 $validator->validate('emails.txt');
