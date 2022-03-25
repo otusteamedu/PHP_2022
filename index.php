9981387b4de47ce99a463556eaa4c46dd0fa009f
viktor <gmac.net.ru@gmail.com>
2022-03-25 13:06:53 +0300

    Правки по кодревью

--- index.php
+++ index.php
@@ -6,7 +6,5 @@ require_once './vendor/autoload.php';
 
 $fileManager = new App\FileManager\FileManager();
 
-$inputName = trim($argv[1]);
-
 $validator = ValidatorFactory::createByEmail();
-$validator->validate($inputName);
+$validator->validate('emails.txt');
