7e7c9d4b8d356878f84fcff4c0f08be158d30ae3
Павел <p.gaponenko@worksolutions.ru>
2022-03-27 00:10:16 +0300

    PGaponenko/hw12. Исправлено получение контейнера

--- elastic_app/app/src/Model/Factory/ElasticModelFactory.php
+++ elastic_app/app/src/Model/Factory/ElasticModelFactory.php
@@ -2,27 +2,22 @@
 
 namespace Elastic\App\Model\Factory;
 
-use DI\ContainerBuilder;
 use Elastic\App\Model\Channel;
 use Elastic\App\Model\ElasticModel;
 use Elastic\App\Model\Video;
+use Elastic\App\Trait\ContainerFactory;
 
 class ElasticModelFactory
 {
+    use ContainerFactory;
+
     private const MAP = [
         'channel' => Channel::class,
         'video' => Video::class,
     ];
 
-    private ContainerBuilder $builder;
-
-    public function __construct(ContainerBuilder $builder)
-    {
-        $this->builder = $builder;
-    }
-
     public function get(string $name): ElasticModel
     {
-        return $this->builder->build()->get(self::MAP[$name]);
+        return $this->getContainer()->get(self::MAP[$name]);
     }
 }
