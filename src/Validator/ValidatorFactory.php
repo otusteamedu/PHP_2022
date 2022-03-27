aed8ee78131b55736daf13d90c0b8e75d420bb4d
ilia <sidor1989@yandex.ru>
2022-03-27 21:40:39 +0300

    hw5

---
+++ src/Validator/ValidatorFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Validator;
+
+use App\FileManager\FileManager;
+use App\Validator\Contracts\ValidatorInterface;
+use App\Validator\Providers\EmailValidator;
+
+class ValidatorFactory
+{
+    /**
+     * create
+     *
+     * @param  mixed $validator
+     * @return Validator
+     */
+    public static function create(ValidatorInterface $validator): Validator
+    {
+        $fileManager = new FileManager();
+
+        return new Validator($fileManager, $validator);
+    }
+
+    
+    /**
+     * createByEmail
+     *
+     * @return Validator
+     */
+    public static function createByEmail(): Validator
+    {
+        return static::create(new EmailValidator());
+    }
+}
