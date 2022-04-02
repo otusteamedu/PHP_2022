c0add2e0916eb2ee2bde07b86198183a5001c974
viktor <gmac.net.ru@gmail.com>
2022-04-02 18:16:19 +0300

    правки по кодревью

--- public/index.php
+++ public/index.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\ExceptionHandler;
-use App\Http\Response;
-
 require_once './../vendor/autoload.php';
-
-$handler = new ExceptionHandler();
 
 $app = new \App\App();
 $response = $app->handle();
--- src/App.php
+++ src/App.php
@@ -3,10 +3,18 @@
 namespace App;
 
 use App\Http\Controller;
+use App\Http\ExceptionHandler;
 use App\Http\Response;
 
 class App
 {
+    private ExceptionHandler $handler;
+
+    public function __construct()
+    {
+        $this->handler = new ExceptionHandler();
+    }
+
     public function handle(): Response
     {
         // по факту тут должен вызываться роутер, но чтобы лишнего не пилить
