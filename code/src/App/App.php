0c032ac946fb728021317c26743eda39b8d45341
Ekaterina Razdorova <e.razdorova@gmail.com>
2022-04-03 00:12:54 +0300

    правки, доработки, улучшения

--- code/src/App/App.php
+++ code/src/App/App.php
@@ -5,25 +5,10 @@ declare(strict_types=1);
 namespace Ekaterina\Hw4\App;
 
 use Ekaterina\Hw4\Controllers\MainController;
-use Ekaterina\Hw4\Http\Response;
-use Ekaterina\Hw4\View\View;
-use Ekaterina\Hw4\Http\Request;
-use Ekaterina\Hw4\ServerData\ServerData;
-use Ekaterina\Hw4\Validator\BracketsValidator;
 use Exception;
 
 class App
 {
-    /**
-     * @var View шаблон вывода
-     */
-    protected View $view;
-
-    /**
-     * @var Request|null Обертка над Request
-     */
-    protected ?Request $request = null;
-
     /**
      * @var MainController|null Контроллер
      */
@@ -36,13 +21,6 @@ class App
     {
         try {
             $this->controller = new MainController();
-            //$this->request = new Request();
-            //$this->view = new View();
-            //if ($this->request->firstStart) {
-            //    $this->view = new View('Вас приветствует валидатор скобочек');
-            //} else {
-            //    $this->view = new View();
-            //}
         } catch (Exception $e) {
             MainController::error($e->getMessage());
         }
@@ -58,15 +36,5 @@ class App
         if ($this->controller instanceof MainController) {
             $this->controller->page();
         }
-
-        //if ($this->request->firstStart) {
-        //    $this->view->page(new ServerData());
-        //} elseif (!empty($this->request->fields)) {
-        //    $brackets = new BracketsValidator($this->request->fields);
-        //    Response::setResponseCode($brackets->validate());
-        //    $this->view->result(new ServerData(), $brackets);
-        //} else {
-        //    Response::setResponseCode(false);
-        //}
     }
 }
