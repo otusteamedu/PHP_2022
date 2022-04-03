28803c6145a96891f3fcf89c24e2f73de2084ead
Павел <p.gaponenko@worksolutions.ru>
2022-04-03 17:49:47 +0300

    PGaponenko/hw13. Реализован сервис для работы с событием

---
+++ redis_app/app/src/Service/EventService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Redis\App\Service;
+
+use Redis\App\Model\Event;
+use Redis\App\Repository\Factory\RepositoryFactory;
+use Redis\App\Repository\Repository;
+
+class EventService
+{
+    private Repository $repository;
+
+    public function __construct(RepositoryFactory $repositoryFactory)
+    {
+        $this->repository = $repositoryFactory->create();
+    }
+
+    public function addEvent(Event $event): string
+    {
+        return $this->repository->add($event)
+            ? 'Событие добавлено'
+            : 'Событие не добавлено';
+    }
+
+    public function deleteEvents(): void
+    {
+        $this->repository->delete();
+    }
+
+    public function getEvent(array $request): Event
+    {
+        return $this->repository->get($request);
+    }
+}
