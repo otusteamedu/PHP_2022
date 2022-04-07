a9fc065c0f3794ed30954228510eceda74eba9aa
decole <decole2014@yandex.ru>
2022-04-08 01:15:09 +0300

    add EL repository and command to create data in EL

---
+++ app/Dtos/VideoDto.php
@@ -0,0 +1,16 @@
+<?php
+
+class VideoDto
+{
+    public string $id;
+
+    public string $videoTitle;
+
+    public string $videoSeconds;
+
+    public string $description = '';
+
+    public int $like = 0;
+
+    public int $dislike = 0;
+}
