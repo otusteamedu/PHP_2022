87b2336f8ceec19f2de54a211ab662efb7bba1ff
viktor <gmac.net.ru@gmail.com>
2022-04-27 00:05:16 +0300

    правки по ревью

--- request.sql
+++ request.sql
@@ -1,5 +1,5 @@
-Select (count(t.session_id) * s.price) as sum, s.film_id, f.title
+Select sum(t.price) as sum, s.film_id, f.title
 from tickets as t
          Join sessions as s ON(t.session_id = s.film_id)
          Join films as f ON(f.id = s.id)
-group by s.price, s.film_id, f.title Order by sum DESC LIMIT 1;
+group by s.film_id, f.title Order by sum DESC limit 1;
