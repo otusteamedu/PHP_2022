397c1f3f5152b21bf33bf31ed2e942d762b86181
decole <decole2014@yandex.ru>
2022-04-27 11:13:08 +0300

    code review

--- code/index.php
+++ code/index.php
@@ -6,10 +6,6 @@ use Klein\Klein;
 
 require __DIR__ . '/src/bootstrap/bootstrap.php';
 
-$env = Dotenv::createImmutable(__DIR__ . '/');
-$env->load();
-$env->required(['STORAGE']);
-
 $router = new Klein();
 
 $api = require(__DIR__ . '/src/Routes/api.php');
--- code/src/bootstrap/bootstrap.php
+++ code/src/bootstrap/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+use Dotenv\Dotenv;
+
 session_start();
 
 require 'vendor/autoload.php';
+
+$env = Dotenv::createImmutable(__DIR__ . '/../../');
+$env->load();
+$env->required(['STORAGE']);
