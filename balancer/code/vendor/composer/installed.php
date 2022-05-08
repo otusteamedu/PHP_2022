54db44a2135da1dc049a8cd4a9e366821f2b7a1b
Igor Shkatov <shkatov.develop@gmail.com>
2022-05-08 13:41:21 +0300

    create a Balancer and req/res HTTP clirnt

---
+++ balancer/code/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '754035088ae8e91ec277f893dda414b33756a0a3',
+        'name' => 'igor/hw4',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'igor/hw4' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '754035088ae8e91ec277f893dda414b33756a0a3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
