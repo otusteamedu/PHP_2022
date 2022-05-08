eef0a9411482acb6c38dd800f408e1cad39c3b85
Igor Shkatov <shkatov.develop@gmail.com>
2022-05-08 14:08:33 +0300

    init app and create structure

---
+++ verificator/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6f63bba599e3f48a8b6a1ca1aeba7f01cc942488',
+        'name' => 'igor/hw5',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'igor/hw5' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6f63bba599e3f48a8b6a1ca1aeba7f01cc942488',
+            'dev_requirement' => false,
+        ),
+    ),
+);
