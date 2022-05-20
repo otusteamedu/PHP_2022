413896407771837e187b0edee3404a9093c3f9fc
Kirill <kirill.kirillov@itglobal.com>
2022-05-20 10:58:18 +0300

    fixes

--- sql/create_table_eav.sql
+++ sql/create_table_eav.sql
@@ -3,13 +3,13 @@ CREATE TAbLE IF NOT EXISTS film (
     name text NOT NULL,
     duration time NOT NULL
 );
-CREATE INDEX ON film(id)
+CREATE INDEX ON film(id);
 
 CREATE TABLE IF NOT EXISTS film_attribute_type (
     id SERIAL PRIMARY KEY NOT NULL,
     type varchar NOT NULL UNIQUE
 );
-CREATE INDEX ON film_attribute_type(id)
+CREATE INDEX ON film_attribute_type(id);
 
 CREATE TABLE IF NOT EXISTS film_attribute (
     id SERIAL NOT NULL PRIMARY KEY,
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS film_attribute (
     name VARCHAR
 );
 
-CREATE INDEX ON film_attribute(id)
+CREATE INDEX ON film_attribute(id);
 
 
 CREATE TABLE IF NOT EXISTS film_attribute_value (
@@ -25,10 +25,10 @@ CREATE TABLE IF NOT EXISTS film_attribute_value (
     film_id int NOT NULL REFERENCES film,
     film_attribute_id int NOT NULL REFERENCES film_attribute,
     val_int INT,
-    val_numeric NUMERIC,
+    val_float float,
     val_varchar VARCHAR(100),
     val_text TEXT,
     val_boolean BOOLEAN,
     val_date DATE
 );
-CREATE INDEX ON film_attribute_value(id)
+CREATE INDEX ON film_attribute_value(id);
--- sql/film_attr_view.sql
+++ sql/film_attr_view.sql
@@ -2,11 +2,11 @@ CREATE VIEW attributes_films AS
 SELECT film.name as film_name,
        film_attribute.name as attribute_name,
        film_attribute_type.type as type,
-       concat_ws('', film_value.value_date, film_value.value_text, film_value.value_boolean) as value
+       concat_ws('', film_attribute_value.val_date, film_attribute_value.val_text, film_attribute_value.val_boolean) as value
 FROM film,
      film_attribute_type,
      film_attribute,
-     film_value
-WHERE film_value.film_id = film.id
-  AND film_value.film_attribute_id = film_attribute.id
+     film_attribute_value
+WHERE film_attribute_value.film_id = film.id
+  AND film_attribute_value.film_attribute_id = film_attribute.id
   AND film_attribute.film_attribute_type_id = film_attribute_type.id;
