6dc057b5e4ab7e4a6c74e64d70164a086dfc9788
Olga Svistunova <olelishna@gmail.com>
2022-05-22 16:33:11 +0800

    Implements brackets checker

--- code/index.php
+++ code/index.php
@@ -1,5 +1,55 @@
 <?php
 
-echo "<h1>Hello, world of balance {$_SERVER['HOSTNAME']}</h1>";
+// Checking current host
+header("Instance: ".$_SERVER['HOSTNAME']);
 
-phpinfo();
+if (isset($_POST['string'])) {
+
+    if (isBalanced($_POST['string'])) {
+        echo "Brackets are in order";
+    } else {
+        http_response_code(400);
+        echo "Brackets are not in order";
+    }
+
+    return;
+}
+
+http_response_code(400);
+echo "Empty string parameter";
+
+//======
+
+function isBalanced($str): bool
+{
+    $str = getBrackets($str);
+
+    return areBracketsInOrder($str);
+}
+
+function getBrackets($str): string
+{
+    $re = "/[^()\[\]{}]/";
+
+    return preg_replace($re, '', $str);
+}
+
+function areBracketsInOrder($str): bool
+{
+    $len = strlen($str);
+
+    $bracket = ["]" => "[", "}" => "{", ")" => "("];
+
+    $openBrackets = [];
+    $isClean = true;
+
+    for ($i = 0; $isClean && $i < $len; $i++) {
+        if (array_key_exists($str[$i], $bracket)) { // found closing bracket
+            $isClean = (array_pop($openBrackets) === $bracket[$str[$i]]);
+        } else {
+            $openBrackets[] = $str[$i];
+        }
+    }
+
+    return $isClean && (count($openBrackets) === 0);
+}
