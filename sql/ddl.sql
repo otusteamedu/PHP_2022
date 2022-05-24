74b1c337b00c8132b60dde91d03168b09e9feaf1
Kirill <kirill.kirillov@itglobal.com>
2022-05-24 12:43:04 +0300

    revew

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -20,26 +20,36 @@ CREATE TABLE IF NOT EXISTS customer (
     phone numeric
 );
 
-CREATE TABLE IF NOT EXISTS ticket (
+CREATE TABLE IF NOT EXISTS ticket_type (
     id SERIAL PRIMARY KEY NOT NULL,
     type VARCHAR(100),
     price int
 );
 
-CREATE TABLE hall_session(
+CREATE TABLE IF NOT EXISTS hall_session(
     session_id int REFERENCES session,
     hall_id int REFERENCES hall
 );
 
-CREATE TABLE IF NOT EXISTS customer_ticket (
+CREATE TABLE IF NOT EXISTS ticket (
     id SERIAL PRIMARY KEY NOT NULL,
     session_id int REFERENCES session,
     customer_id int REFERENCES customer,
-    ticket_id int REFERENCES ticket,
-    place smallint NOT NULL CHECK
-        (place > 0 AND place < 30),
-    row smallint NOT NULL CHECK
-        (row > 0 AND row < 15)
+    ticket_type_id int REFERENCES ticket_type,
+    place_id smallint NOT NULL,
+    row_id smallint NOT NULL
 );
 
-CREATE UNIQUE INDEX ON customer_ticket(session_id, place, row);
+CREATE TABLE IF NOT EXISTS place (
+    id SERIAL NOT NULL PRIMARY KEY,
+    number smallint NOT NULL CHECK
+        (number > 0 AND number < 30)
+);
+
+CREATE TABLE IF NOT EXISTS row (
+    id SERIAL NOT NULL PRIMARY KEY,
+    number smallint NOT NULL CHECK
+        (number > 0 AND number < 15)
+);
+
+CREATE UNIQUE INDEX ON ticket(session_id, place_id, row_id);
