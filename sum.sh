36de1c329c466dcc776834a9bb6226e8027cd8f9
Weipy <nikita.savelev1999@mail.ru>
2022-06-01 22:31:35 +0300

    NSavelev/hw2. function of validation argument as number was added.

--- sum.sh
+++ sum.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function numberValidator {
+  if [[ $(echo "$1" | awk '/^-?[0-9]*[.,]?[0-9]+$/{print $0}' | wc -l) -eq '0' ]] ; then
+    echo "Аргумент $1 должен быть числом"
+    exit
+  fi
+}
+
 if [ $# -eq 0 ]; then
   echo "Введите аргументы"
   exit
@@ -10,15 +17,9 @@ if [ -z "$2" ]; then
   exit
 fi
 
-if [[ $(echo "$1" | awk '/^-?[0-9]*[.,]?[0-9]+$/{print $0}' | wc -l) -eq '0' ]] ; then
-  echo "Аргументы должны быть числами";
-  exit;
-fi
+numberValidator "$1"
 
-if [[ $(echo $2 | awk '/^-?[0-9]*[.,]?[0-9]+$/{print $0}' | wc -l) -eq '0' ]] ; then
-  echo "Аргументы должны быть числами";
-  exit;
-fi
+numberValidator "$2"
 
 sum=$(echo "$1 $2" | awk '{print $1 + $2}')
 
