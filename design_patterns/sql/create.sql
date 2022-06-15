108e43eb9c64c01a70e1dd297366b6276d09dbcd
Павел <p.gaponenko@worksolutions.ru>
2022-06-15 20:56:33 +0300

    PGaponenko/hw19. Реализована инфраструктура

---
+++ design_patterns/sql/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE money (
+    id SERIAL PRIMARY KEY,
+    fifty_banknote_count FLOAT,
+    hundred_banknote_count FLOAT,
+    five_hundred_banknote_count FLOAT,
+    thousand_banknote_count FLOAT
+);
