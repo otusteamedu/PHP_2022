747e01fd5a0ad107a0611aaf1cdbd14df77c6ca1
Pavel Shilyaev <pashilyaev@yandex.ru>
2022-06-27 19:23:01 +0300

    Normalization

--- OTUS_HW8/ddl.sql
+++ OTUS_HW8/ddl.sql
@@ -23,6 +23,13 @@ CREATE SEQUENCE public.seanses_id_seq
 	MINVALUE 1
 	MAXVALUE 9223372036854775807
 	START 1;
+-- DROP SEQUENCE public.seats_id_seq;
+
+CREATE SEQUENCE public.seats_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1;
 -- DROP SEQUENCE public.tickets_id_seq;
 
 CREATE SEQUENCE public.tickets_id_seq
@@ -38,6 +45,7 @@ CREATE SEQUENCE public.tickets_id_seq
 CREATE TABLE public.films (
 	id serial4 NOT NULL,
 	film_name varchar NOT NULL,
+	base_ticket_price numeric NOT NULL DEFAULT 150,
 	CONSTRAINT films_pk PRIMARY KEY (id)
 );
 
@@ -51,8 +59,20 @@ CREATE TABLE public.films (
 CREATE TABLE public.halls (
 	id serial4 NOT NULL,
 	hall_name varchar NOT NULL,
-	number_of_seats int4 NOT NULL,
 	CONSTRAINT halls_pk PRIMARY KEY (id)
+);
+
+
+-- public.price_description definition
+
+-- Drop table
+
+-- DROP TABLE public.price_description;
+
+CREATE TABLE public.price_description (
+	id serial4 NOT NULL,
+	description varchar NOT NULL,
+	CONSTRAINT price_description_pk PRIMARY KEY (id)
 );
 
 
@@ -64,17 +84,46 @@ CREATE TABLE public.halls (
 
 CREATE TABLE public.seanses (
 	id serial4 NOT NULL,
-	hall_id int4 NOT NULL,
 	film_id int4 NOT NULL,
 	start_time timestamp NOT NULL,
 	end_time timestamp NOT NULL,
 	CONSTRAINT seanses_check CHECK ((start_time < end_time)),
 	CONSTRAINT seanses_pk PRIMARY KEY (id),
-	CONSTRAINT seanses_fk FOREIGN KEY (film_id) REFERENCES public.films(id),
-	CONSTRAINT seanses_fk_1 FOREIGN KEY (hall_id) REFERENCES public.halls(id)
+	CONSTRAINT seanses_fk FOREIGN KEY (film_id) REFERENCES public.films(id)
 );
 CREATE INDEX seanses_film_id_idx ON seanses USING btree (film_id);
-CREATE INDEX seanses_hall_id_idx ON seanses USING btree (hall_id);
+
+
+-- public.seats definition
+
+-- Drop table
+
+-- DROP TABLE public.seats;
+
+CREATE TABLE public.seats (
+	id serial4 NOT NULL,
+	hall_id int4 NOT NULL,
+	"row_number" int4 NOT NULL,
+	seat_number int4 NOT NULL,
+	vip bool NOT NULL DEFAULT false,
+	CONSTRAINT seats_pk PRIMARY KEY (id),
+	CONSTRAINT seats_fk FOREIGN KEY (hall_id) REFERENCES public.halls(id)
+);
+
+
+-- public.prices definition
+
+-- Drop table
+
+-- DROP TABLE public.prices;
+
+CREATE TABLE public.prices (
+	id serial4 NOT NULL,
+	value numeric NOT NULL,
+	description_id int4 NOT NULL,
+	CONSTRAINT prices_pk PRIMARY KEY (id),
+	CONSTRAINT prices_fk FOREIGN KEY (id) REFERENCES public.price_description(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
 
 
 -- public.tickets definition
@@ -86,12 +135,13 @@ CREATE INDEX seanses_hall_id_idx ON seanses USING btree (hall_id);
 CREATE TABLE public.tickets (
 	id serial4 NOT NULL,
 	sold bool NOT NULL DEFAULT true,
-	price numeric NOT NULL,
-	"row" int4 NOT NULL,
-	col int4 NOT NULL,
 	seanse_id int4 NOT NULL,
+	seat_id int4 NOT NULL,
+	price_id int4 NOT NULL,
 	CONSTRAINT tickets_pk PRIMARY KEY (id),
-	CONSTRAINT tickets_fk FOREIGN KEY (seanse_id) REFERENCES public.seanses(id)
+	CONSTRAINT tickets_fk FOREIGN KEY (seanse_id) REFERENCES public.seanses(id),
+	CONSTRAINT tickets_prices_fk FOREIGN KEY (id) REFERENCES public.prices(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT tickets_seats_fk FOREIGN KEY (seat_id) REFERENCES public.seats(id)
 );
 CREATE INDEX tickets_seanse_id_idx ON tickets USING btree (seanse_id);
 
