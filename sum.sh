eeabb1a6fe34404ece8c0f326b4f4693516cb3d6
Anton Shishak <ashishak@awardwallet.com>
2022-08-10 22:48:15 +0300

    Домашнее задание №2

---
+++ sum.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+regex='^([0-9\.\-]+)$';
+
+badRegexp='^[0-9]+\,[0-9]+$';
+
+if [[ $1 =~ $badRegexp ]] || [[ $2 =~ $badRegexp ]]; then
+  echo "Ошибка ввода: Дробный числа записываются через точку";
+  exit 1;
+fi
+
+
+if ! [[ $1 =~ $regex ]]; then
+  echo "Первое значение не число";
+  exit 1;
+fi
+
+if ! [[ $1 =~ $regex ]]; then
+  echo "Первое значение не число";
+  exit 1;
+fi
+
+if ! [[ $2 =~ $regex ]]; then
+  echo "Второе значение не число";
+  exit 1;
+fi
+
+ echo "Cумма чисел $1 + $2, равна:"
+ echo $1+$2 | awk "BEGIN{ print $1 + $2}";
