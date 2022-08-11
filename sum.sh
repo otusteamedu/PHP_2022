634d173f0c44cf4328ff3fa727042b6b7c973ade
Anton Shishak <ashishak@awardwallet.com>
2022-08-11 22:23:55 +0300

    Домашнее задание №2 - актуальная версия

--- sum.sh
+++ sum.sh
@@ -15,15 +15,10 @@ if ! [[ $1 =~ $regex ]]; then
   exit 1;
 fi
 
-if ! [[ $1 =~ $regex ]]; then
-  echo "Первое значение не число";
-  exit 1;
-fi
-
 if ! [[ $2 =~ $regex ]]; then
   echo "Второе значение не число";
   exit 1;
 fi
 
  echo "Cумма чисел $1 + $2, равна:"
- echo $1+$2 | awk "BEGIN{ print $1 + $2}";
+ awk "BEGIN{ print $1 + $2}";
