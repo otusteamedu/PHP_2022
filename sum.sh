4d4396b4f14202403d99a062d6ef380675aad3fc
Dmitry Fedoroff <lindrid32@gmail.com>
2022-08-17 10:41:33 +1000

    hw2

--- sum.sh
+++ sum.sh
@@ -5,4 +5,12 @@ if [ "$#" != 2 ]; then
   exit 1
 fi
 
-echo "$1 + $2" | bc
+for arg in "$@"
+do
+  if [[ "$arg" != $(echo -e "$arg" | grep -o '[0-9.-]\+') ]]; then
+    echo "$arg is not a valid number"
+    exit 1
+  fi
+done
+
+awk "BEGIN {print $1 + $2}"
