d28a3a2d20b22b5d36184e2ed0c99b83b83e36ad
mishaikon@gmail.com <7tiou3tlet>
2022-08-19 13:08:55 +0300

    Clean my repository

--- hw2_linux/cmd_freq.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# print list of frequently used bash commands
-history | awk '($2 ~ /^[[:alnum:]]+$/) { ++a[$2]; t = length($2); if (t > l) l = t; } END { for (i in a) printf("%s%" (l - length(i) + 1) "s%5.2f%%\n", i, " ", (a[i] * 100 / NR)); }'
