ff81f79c172d0a8d065f2c9c7a985f14a794bc4b
Igoncharov <studio@webboss.pro>
2022-08-25 02:30:27 +0300

    psr-4 test autoload

---
+++ public/app/Libs/Runner.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wbh\App\Libs;
+
+class Runner
+{
+    public function __construct()
+    {
+        echo "I am test Class";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+use Wbh\App\Libs\Runner;
+
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+
+new Runner();
