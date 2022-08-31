2c05b6220a4496df0502ca30f3132f0698b4f6b5
Igor Tsyganok <ciganokigor@gmail.com>
2022-08-31 20:04:32 +0500

    hw3 все равно не работает...

--- code/index.php
+++ code/index.php
@@ -1,5 +1,45 @@
-<?php
+<html lang="en">
 
-echo "Привет, Otus!<br>".date("Y-m-d H:i:s")."<br><br>";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OTUS_hw4</title>
+</head>
 
-echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
+<body>
+    <?php
+    if (!empty($_POST['filter_string'])) {
+        $string = $_POST['filter_string'];
+        $counter = 0;
+        $openBracket = ['('];
+        $closedBracket = [')'];
+        $length = strlen($string);
+        for($i = 0; $i<$length; $i++) {
+            $char = $string[$i];
+            if(in_array($char, $openBracket)) {
+                $counter ++;
+            } elseif (in_array($char, $closedBracket)) {
+                $counter --;
+            }
+        }
+        if($counter != 0) {
+            echo "Количество скобочек не верно";
+        } else {
+            echo "Количество скобочек верно";
+        }
+    } else {
+        echo "Строка пустая: {$_POST['filter_string']}";
+    }
+    ?>
+
+<hr>
+<h3>Проверка входных данных</h3>
+<form method="post">
+    <label for="filter_string">Укажите название:</label>
+    <input type="text" name="filter_string" /><br>
+    <input type="submit" value="Проверить">
+</form>
+<hr>
+</body>
+</html>
