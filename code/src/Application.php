ffffa314800de1d2713a1bdd754848aeee8b96e5
Nikolai Timakov <ntimakov@mail.ru>
2022-09-13 13:08:58 +0300

    Удаление побочного эффекта

--- code/src/Application.php
+++ code/src/Application.php
@@ -4,17 +4,34 @@ declare(strict_types=1);
 
 namespace Nikolai\Php;
 
+use Nikolai\Php\Configuration\ConfigurationLoaderInterface;
+use Nikolai\Php\Service\Dumper;
+use Nikolai\Php\Service\DumperInterface;
 use Nikolai\Php\Service\EmailsFromFileVerifierService;
 use Nikolai\Php\Service\EmailVerifier;
 use Nikolai\Php\Configuration;
 
 class Application implements ApplicationInterface
 {
+    const RESULT_HEADER = 'Результаты проверки';
+    const EXCEPTION_HEADER = 'Исключение';
+    const CONFIGURATION_FILE = '/config/services.yaml';
+
+    private DumperInterface $dumper;
+    private ConfigurationLoaderInterface $configurationLoader;
+
+    public function __construct()
+    {
+        $this->dumper = new Dumper();
+        $this->configurationLoader = new Configuration\ConfigurationLoader(
+            dirname(__DIR__) . self::CONFIGURATION_FILE
+        );
+    }
+
     public function run(): void
     {
         try {
-            $configurationLoader = new Configuration\ConfigurationLoader(dirname(__DIR__) . '/config/services.yaml');
-            $configuration = $configurationLoader->load();
+            $configuration = $this->configurationLoader->load();
 
             $stringVerifier = new EmailVerifier();
             $emailsFromFileVerifierService = new EmailsFromFileVerifierService(
@@ -23,13 +40,9 @@ class Application implements ApplicationInterface
             );
 
             $result = $emailsFromFileVerifierService->verify();
-
-            echo '<br>Результаты проверки:<br><pre>';
-            var_dump($result);
-            echo '</pre>';
-
+            $this->dumper->dump(self::RESULT_HEADER, $result);
         } catch (\Exception $exception) {
-            echo '<br>Исключение: ' . $exception->getMessage() . '<br>';
+            $this->dumper->dump(self::EXCEPTION_HEADER, $exception->getMessage());
         }
     }
 }
