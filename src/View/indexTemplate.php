6d7387b5957b78faa1e5cb2c2c0044579568d7c7
Pavel Shilyaev <pashilyaev@yandex.ru>
2022-10-06 19:19:15 +0300

    Удаление лишних файлов на фронте.

--- src/View/indexTemplate.php
+++ src/View/indexTemplate.php
@@ -10,8 +10,8 @@ declare(strict_types=1);
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="/vendor/datepicker/css/datepicker.material.css">
-    <script src="/vendor/datepicker/datepicker.js"></script>
+    <link rel="stylesheet" href="/js/datepicker/css/datepicker.material.css">
+    <script src="/js/datepicker/datepicker.js"></script>
     <title>Получение банковской выписки</title>
 </head>
 <body>
--- src/View/messageTemplate.php
+++ src/View/messageTemplate.php
@@ -11,8 +11,6 @@ declare(strict_types=1);
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="/vendor/datepicker/css/datepicker.material.css">
-    <script src="/vendor/datepicker/datepicker.js"></script>
     <title>Получение банковской выписки</title>
 </head>
 <body>
@@ -21,17 +19,17 @@ declare(strict_types=1);
     <main class="form-signin w-100 m-auto">
         <h1>Получение выписки</h1>
 
-        <?if (isset($data["errors"]) && count($data["errors"])>0) {?>
+        <?php if (isset($data["errors"]) && count($data["errors"])>0) {?>
             <div class="alert alert-danger" role="alert">
-                <?foreach ($data["errors"] as $err){?>
+                <?php foreach ($data["errors"] as $err){?>
                 <p class="error-text"><?=$err?></p>
-                <?}?>
+                <?php }?>
             </div>
-        <?} else {?>
+        <?php } else {?>
             <div class="alert alert-success" role="alert">
             Успешно
         </div>
-        <?}?>
+        <?php }?>
 
         <a href="index.php" class="w-100 btn btn-info btn-lg">На главную</a>
     </main>
