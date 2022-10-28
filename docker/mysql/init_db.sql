ac6147eed9f013f5e5aff2924d95d34b291f3132
Artem <artem@agsys.ru>
2022-10-28 17:55:13 +0300

    completed hw1

---
+++ docker/mysql/init_db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE mysite CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE USER 'admin'@'*' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'*' WITH GRANT OPTION;
+
+GRANT ALL ON mysite.* TO 'admin'@'%' IDENTIFIED BY 'password';
+
+FLUSH PRIVILEGES;
