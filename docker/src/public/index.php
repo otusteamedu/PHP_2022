abdcf625645a7ffb5ca117756ba8fa7ad630edb8
getinweb <79265732767@yandex.ru>
2022-10-29 22:13:19 +0300

    changed index.php according to slimframeworks Hello World

--- docker/src/public/index.php
+++ docker/src/public/index.php
@@ -1,10 +1,26 @@
 <?php
 
-$redis = new Redis();
-$redis->connect('redis');
-echo $redis->ping('Connection to redis success<br>');
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-$mc = new Memcached();
-$mc->addServer('memcached', 11211);
-$mc->set('Memcached', 'Connection to memcached success<br>');
-echo $mc->get('Memcached');
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response) {
+    $redis = new Redis();
+    $redis->connect('redis');
+    $answerFromRedis = $redis->ping('Connection to Redis success<br>');
+    $response->getBody()->write($answerFromRedis);
+
+    $mc = new Memcached();
+    $mc->addServer('memcached', 11211);
+    $mc->set('Memcached', 'Connection to Memcached success<br>');
+    $answerFromMemcached = $mc->get('Memcached');
+    $response->getBody()->write($answerFromMemcached);
+
+    return $response;
+});
+
+$app->run();
