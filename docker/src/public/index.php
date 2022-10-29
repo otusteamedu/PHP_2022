b6fab718862739d98dea4d3641f174a38f3bad93
getinweb <79265732767@yandex.ru>
2022-10-29 21:30:51 +0300

    check connections to redis & memcached in index.php

--- docker/src/public/index.php
+++ docker/src/public/index.php
@@ -1,3 +1,10 @@
 <?php
 
-phpinfo();
+$redis = new Redis();
+$redis->connect('redis');
+echo $redis->ping('Connection to redis success<br>');
+
+$mc = new Memcached();
+$mc->addServer('memcached', 11211);
+$mc->set('Memcached', 'Connection to memcached success<br>');
+echo $mc->get('Memcached');
