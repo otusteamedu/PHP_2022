f910e4f99052d0419b72ee8016c5a6e1c88ecb15
chernysh <mihail.chernysh@gmail.com>
2022-10-31 11:40:59 +0700

    docker compose + php:7.4-fpm

---
+++ app/index.php
@@ -0,0 +1,8 @@
+<h1>Hello World</h1>
+<p>Dramatically grow resource-leveling infomediaries for sticky alignments. Dramatically iterate.</p>
+<p>Appropriately leverage existing customer directed meta-services and collaborative value.</p>
+<p>Professionally seize end-to-end innovation rather than global growth strategies.</p>
+
+<?php
+
+phpinfo();
