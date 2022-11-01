cf1c0b53e27a0fc47f3a030efb782c3e0dfaca04
getinweb <79265732767@yandex.ru>
2022-11-01 12:08:45 +0300

    added database connection test to index.php, wrapped all connections in try catch blocks

--- docker/src/public/index.php
+++ docker/src/public/index.php
@@ -9,16 +9,36 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
+
+    try {
+        $dsn = 'pgsql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME') . ';port=' . getenv('DB_PORT') . ';';
+        new PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'));
+        $response->getBody()->write('Successful database connection<br>');
+    } catch (PDOException $e) {
+        $response->getBody()->write('Error connecting to database: ');
+        $response->getBody()->write($e->getMessage() . '<br>');
+    }
+
     $redis = new Redis();
-    $redis->connect('redis');
-    $answerFromRedis = $redis->ping('Connection to Redis success<br>');
-    $response->getBody()->write($answerFromRedis);
+    try {
+        $redis->connect('redis', getenv('REDIS_PORT'));
+        $answerFromRedis = $redis->ping('Successful Redis connection<br>');
+        $response->getBody()->write($answerFromRedis);
+    } catch (RedisException $e) {
+        $response->getBody()->write('Error connecting to Redis: ');
+        $response->getBody()->write($e->getMessage() . '<br>');
+    }
 
     $mc = new Memcached();
-    $mc->addServer('memcached', 11211);
-    $mc->set('Memcached', 'Connection to Memcached success<br>');
-    $answerFromMemcached = $mc->get('Memcached');
-    $response->getBody()->write($answerFromMemcached);
+    try {
+        $mc->addServer('memcached', getenv('MEMCACHED_PORT'));
+        $mc->set('Memcached', 'Successful Memcached connection<br>');
+        $answerFromMemcached = $mc->get('Memcached');
+        $response->getBody()->write($answerFromMemcached);
+    } catch (MemcachedException $e) {
+        $response->getBody()->write('Error connecting to Memcached: ');
+        $response->getBody()->write($e->getMessage());
+    }
 
     return $response;
 });
