278349d8a544d26ce13d809ce5387f711895dbe5
pozhidaev_p <pozhidaevpro@gmail.com>
2022-11-06 10:55:34 +0300

    hw2_v2

--- sum.sh
+++ sum.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-#not bc package check
-if ! command -v bc &> /dev/null
-then
-    echo "Error:Not install bc package"
-    exit
-fi
-
-#empty check
-if [  -z "$1" ];then
-  echo "Error:The first argument is empty"
-  exit
-elif [  -z "$2" ];then
-  echo "Error:The second argument is empty"
-  exit
-fi
-
 #not number check
 pattern="^[+-]?[0-9]+([.][0-9]+)?$"
 if ! [[ $1 =~ $pattern ]];then
@@ -26,5 +10,14 @@ elif ! [[ $2 =~ $pattern ]];then
   exit
 fi
 
+#not bc package check
+if command -v bc &> /dev/null
+then
+    echo "Result is `echo "$1+$2" | bc`"
+else
+    echo "Result is `awk "BEGIN {print $1+$2}"`"
+fi
 
-echo "Result is `echo "$1+$2" | bc`"
+
+
+
