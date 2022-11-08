c1763266ca0b1ad0f524e8f513b1723d1248551a
chernysh <mihail.chernysh@gmail.com>
2022-11-08 09:13:04 +0700

    Вариант без bc

--- sum.sh
+++ sum.sh
@@ -17,36 +17,5 @@ fi
 bc <<< "$FIRST_NUMBER+$SECOND_NUMBER"
 
 # Вариант без bc
-#cut -d . -f 2 <<< "345.657"
-IFS='.' read -r -a split <<< "$FIRST_NUMBER"
-FIRST_NUMBER=${split[0]}${split[1]}
-FIRST_DECIMAL=${#split[1]}
-
-IFS='.' read -r -a split <<< "$SECOND_NUMBER"
-SECOND_NUMBER=${split[0]}${split[1]}
-SECOND_DECIMAL=${#split[1]}
-
-# добиваемся единой разрядности
-if [ $FIRST_DECIMAL -gt $SECOND_DECIMAL ]; then
-  DECIMAL=$FIRST_DECIMAL
-  DIFF=$(($FIRST_DECIMAL-$SECOND_DECIMAL))
-  for i in $(seq $DIFF); do
-    SECOND_NUMBER="${SECOND_NUMBER}0"
-  done
-fi
-
-# добиваемся единой разрядности
-if [ $SECOND_DECIMAL -gt $FIRST_DECIMAL ]; then
-  DECIMAL=$SECOND_DECIMAL
-  DIFF=$(($SECOND_DECIMAL-$FIRST_DECIMAL))
-  for i in $(seq $DIFF); do
-    FIRST_NUMBER="${FIRST_NUMBER}0"
-  done
-fi
-
-SUMMA=$(($FIRST_NUMBER+$SECOND_NUMBER))
-COUNT_SYMBOLS=$((${#SUMMA}-DECIMAL))
-COUNT_SYMBOLS_NEXT=$(($COUNT_SYMBOLS+1))
-
-echo $( echo $SUMMA | cut -b -$COUNT_SYMBOLS)'.'$( echo $SUMMA | cut -c $COUNT_SYMBOLS_NEXT-)
+echo $FIRST_NUMBER' '$SECOND_NUMBER | awk -F' ' '{sum+=$1+$2;} END{print sum;}'
 
