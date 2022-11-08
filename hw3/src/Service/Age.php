72c0fc4dfef6b03976312d0a6be6ad848edac853
Вера Аджиева <v.adzhieva@intec-balance.ru>
2022-11-08 23:11:24 +0300

    Подключение к проекту своей библиотеки и баблиотеки Carbon

---
+++ hw3/src/Service/Age.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace VeraAdzhieva\Hw3\Service;
+
+use Carbon\Carbon;
+
+class Age
+{
+    /*
+     * Возраст.
+     *
+     * @param int $year Год.
+     * @param int $month Месяц.
+     * @param int $day День.
+     *
+     * @return int Возраст.
+     */
+    public function getAge(int $year, int $month, int $day): int
+    {
+        return Carbon::createFromDate($year, $month, $day)->age;
+    }
+
+    /*
+     * Сравнение дат.
+     *
+     * @param int $year Год.
+     * @param int $month Месяц.
+     * @param int $day День.
+     *
+     * @return int Возраст.
+     */
+    public function getDate()
+    {
+        return Carbon::now()->toDateTimeString();
+    }
+
+}
