26353e558e0316cb931601cbd00e88a120b522ac
chernysh <mihail.chernysh@gmail.com>
2022-11-08 10:30:20 +0700

    Redis

--- app/index.php
+++ app/index.php
@@ -4,15 +4,14 @@
 $memcacheD = new Memcached;
 $memcacheD->addServer(getenv('MEMCACHED_HOST'), getenv('MEMCACHED_PORT'));
 $memcacheD->set('random_num', rand());
-var_dump($memcacheD->get('random_num'));
+echo sprintf('Memcached: %d', $memcacheD->get('random_num'));
+echo '<br />';
 
 //Connecting to Redis server on localhost
-//$redis = new Redis();
-//$redis->connect('127.0.0.1', 6379);
-//echo "Connection to server sucessfully";
-////check whether server is running or not
-//echo "Server is running: " . $redis->ping();
-
-
+$redis = new Redis();
+$redis->connect(getenv('REDIS_HOST'), getenv('REDIS_PORT'));
+$redis->set('random_num', rand());
+echo sprintf('Redis: %d', $redis->get('random_num'));
+echo '<br />';
 
 //phpinfo();
