011b2f0e353270be709002d226eaa653801bbef6
pingachguk <imayakimov@gmail.com>
2022-11-08 07:52:08 +0300

    Окончательная версия сумматора

--- sum.sh
+++ sum.sh
@@ -2,9 +2,9 @@
 
 a=$1
 b=$2
-CHECK_INPUT=$(echo `echo $a$b | grep '[^0-9]' | wc -l`)
+CHECK_INPUT=$(echo `echo $a$b | grep '\-?\d+(\.\d{0,})?' | wc -l`)
 if [ $CHECK_INPUT -gt 0 ]; then
     echo "Вы ввели некорректное число"
     exit 1
 fi
-echo $(($a+$b))
+echo $(bc <<< $a+$b)
