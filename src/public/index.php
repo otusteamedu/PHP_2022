4be4fc505653efdf18eb87a317ec237a5395973e
getinweb <79265732767@yandex.ru>
2022-11-10 16:49:13 +0300

    added test example for getinweb/test-composer-package

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Getinweb\TestComposerPackage\PrintProcessor;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -9,7 +10,17 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('Message');
+    $arr = [
+        'level1' => [
+            '1' => 1,
+            'level2' => [
+                '2' => 2,
+                'level3' => 3,
+            ],
+        ]
+    ];
+    $formatted_output = PrintProcessor::print_array($arr);
+    $response->getBody()->write($formatted_output);
     return $response;
 });
 
