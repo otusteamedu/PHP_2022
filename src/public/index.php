9926400e14093ad55457dda3201473d63c32cba9
getinweb <79265732767@yandex.ru>
2022-11-11 17:21:31 +0300

    composer removed unnecessary getinweb/test-composer-package

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-use Getinweb\TestComposerPackage\PrintProcessor;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -10,17 +9,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
-    $arr = [
-        'level1' => [
-            '1' => 1,
-            'level2' => [
-                '2' => 2,
-                'level3' => 3,
-            ],
-        ]
-    ];
-    $formatted_output = PrintProcessor::print_array($arr);
-    $response->getBody()->write($formatted_output);
+    $response->getBody()->write('Hello!');
     return $response;
 });
 
