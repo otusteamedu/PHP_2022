fdfc095ff7c44165c0ed89211dd5a29bc1a59a16
getinweb <79265732767@yandex.ru>
2022-11-11 23:53:39 +0300

    fixed routes in index.php

--- src/public/index.php
+++ src/public/index.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\CheckBrackets;
 use App\Http\HomePage;
 use App\Http\SessionTest;
 use Slim\Factory\AppFactory;
@@ -13,6 +14,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', HomePage::class);
+$app->post('/', CheckBrackets::class);
 $app->get('/session-test', SessionTest::class);
 
 $app->run();
