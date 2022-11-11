acff4859f307245d850b9577fa78e0a3d11c48f2
getinweb <79265732767@yandex.ru>
2022-11-11 20:02:01 +0300

    added test for sessions saving

--- src/public/index.php
+++ src/public/index.php
@@ -9,7 +9,23 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('Hello!');
+
+    $message = 'Hello from ' . $_SERVER['SERVER_NAME'] . '<br>';
+
+    $response->getBody()->write($message);
+
+    session_start();
+    if (!isset($_SESSION['visit'])) {
+        $message = "This is the first time you're visiting this server\n";
+        $_SESSION['visit'] = 0;
+    } else {
+        $message = "Your number of visits: " . $_SESSION['visit'] . "\n";
+    }
+
+
+    $_SESSION['visit']++;
+
+    $response->getBody()->write($message);
     return $response;
 });
 
