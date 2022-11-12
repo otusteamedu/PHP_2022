121b03435dc292795c6e9ea55db7b0395afca1d2
nikcrazy37 <nik_crazy@bk.ru>
2022-11-12 18:23:33 +0300

    update to nikcrazy37/composer-package (v1.0.2)

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ require __DIR__ . "/vendor/autoload.php";
 
 use Nikcrazy37\ComposerPackage\StringRedactor;
 
-$stringer = new StringRedactor();
-echo $stringer->setUpper('some text');
+$stringer = new StringRedactor('some text');
+echo $stringer->setUpper();
