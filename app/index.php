6b45a2b0e25fb74c7c54b4e3c050904416d11236
Elias <eliasejump@gmail.com>
2022-11-14 00:26:51 +0300

    simple balanced

--- app/index.php
+++ app/index.php
@@ -5,12 +5,22 @@ declare(strict_types=1);
 use Eliasjump\StringsVerification\Application;
 use Eliasjump\StringsVerification\Controllers\StringController;
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 $app = new Application();
 
 $app->router->get('/', function () {
-    return 'Success';
+    $text = [];
+    session_start();
+
+    $text[] = 'ID сессии: ' . session_id();
+
+    !isset($_SESSION['counter']) ? $_SESSION['counter'] = 1 : $_SESSION['counter']++;
+
+    $text[] = "Счетчик посещений: " . $_SESSION['counter'];
+    $text[] = "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
+
+    return implode("<br/>", $text);
 });
 
 $app->router->post('/', [StringController::class, 'run']);
