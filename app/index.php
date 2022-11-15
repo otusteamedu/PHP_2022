af5cc5005873c85c347ecd8876717855c1548b61
Elias <eliasejump@gmail.com>
2022-11-15 23:36:48 +0300

    create email validate

--- app/index.php
+++ app/index.php
@@ -1,3 +1,56 @@
 <?php
 
-echo "Hello";
+declare(strict_types=1);
+
+use Eliasjump\EmailVerification\EmailValidator;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$emails = isset($_POST['emails']) ? explode("\n", $_POST['emails']) : [];
+$response = [
+    'emails' => $emails,
+    'errors' => []
+];
+
+if (!empty($emails)) {
+    $response['errors'] = EmailValidator::run($emails);
+}
+?>
+<html lang="ru">
+<head>
+    <title>Валидатор Email</title>
+
+    <style>
+        .error {
+            color: darkred;
+        }
+        .ok {
+            color: darkgreen;
+        }
+    </style>
+</head>
+<body>
+<main>
+    <?php
+    if (!empty($response['errors'])) {
+        foreach ($response['errors'] as $error) {
+            echo "<div class='error'>{$error}</div>";
+        }
+    }
+    if (empty($response['errors']) && $response['emails']) {
+        echo '<div class="ok">Все email адреса корректны.</div>';
+    } ?>
+    <form method="post" action="">
+        <div>
+            <p>Введите список email</p>
+            <label>
+                <textarea rows="10" cols="37" name="emails"><?= implode("\n", $emails) ?></textarea>
+            </label>
+        </div>
+        <div>
+            <button type="submit">Проверить</button>
+        </div>
+    </form>
+</main>
+</body>
+</html>
---
+++ app/src/EmailValidator.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eliasjump\EmailVerification;
+
+class EmailValidator
+{
+    const PATTERN = '/^(?=.{1,64}@)[A-Za-z0-9_-]+(.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(.[A-Za-z0-9-]+)*(.[A-Za-z]{2,})$/';
+
+    public static function run(array $emails): array
+    {
+        $errors = [];
+        foreach ($emails as $email) {
+            $email = trim($email);
+            if ($email == '') {
+                $errors[] = "Пустая строка";
+                continue;
+            }
+
+            if (!preg_match(static::PATTERN, $email)) {
+                $errors[] = "Email {$email} - не валидный";
+                continue;
+            }
+            $mx = explode('@', $email)[1];
+            if (!checkdnsrr($mx, 'MX')) {
+                $errors[] = "MX {$mx} - не валидный";
+            }
+        }
+
+        return $errors;
+    }
+}
