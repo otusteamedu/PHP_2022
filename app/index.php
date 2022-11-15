87aa85101932dfdd3c1eba58d65f9cf087576c0e
elias <eliasejump@gmail.com>
2022-11-15 09:32:08 +0300

    update replace session logic

--- app/index.php
+++ app/index.php
@@ -7,16 +7,15 @@ use Eliasjump\StringsVerification\Controllers\StringController;
 
 require __DIR__ . '/vendor/autoload.php';
 
+session_start();
+!isset($_SESSION['counter']) ? $_SESSION['counter'] = 1 : $_SESSION['counter']++;
+
 $app = new Application();
 
 $app->router->get('/', function () {
     $text = [];
-    session_start();
 
     $text[] = 'ID сессии: ' . session_id();
-
-    !isset($_SESSION['counter']) ? $_SESSION['counter'] = 1 : $_SESSION['counter']++;
-
     $text[] = "Счетчик посещений: " . $_SESSION['counter'];
     $text[] = "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
 
