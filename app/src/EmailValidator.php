de8cf7baf79dfe48b4b26de5cd90b9b4e161e366
Elias <eliasejump@gmail.com>
2022-11-15 23:41:15 +0300

    lint-fix

--- app/src/EmailValidator.php
+++ app/src/EmailValidator.php
@@ -6,7 +6,8 @@ namespace Eliasjump\EmailVerification;
 
 class EmailValidator
 {
-    const PATTERN = '/^(?=.{1,64}@)[A-Za-z0-9_-]+(.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(.[A-Za-z0-9-]+)*(.[A-Za-z]{2,})$/';
+    private const PATTERN = '/^(?=.{1,64}@)[A-Za-z0-9_-]+(.[A-Za-z0-9_-]+)*@[^-]' .
+    '[A-Za-z0-9-]+(.[A-Za-z0-9-]+)*(.[A-Za-z]{2,})$/';
 
     public static function run(array $emails): array
     {
