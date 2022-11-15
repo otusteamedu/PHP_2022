ecee99b523e776b38e911df95d74627263b92ef6
getinweb <79265732767@yandex.ru>
2022-11-15 13:43:12 +0300

    moved http_response_code(500) to app

--- src/config/app.php
+++ src/config/app.php
@@ -6,6 +6,7 @@ use Slim\App;
 use Slim\Factory\AppFactory;
 
 return static function (): App {
+    http_response_code(500);
     $app = AppFactory::create();
     (require __DIR__ . '/routes.php')($app);
     return $app;
--- src/public/index.php
+++ src/public/index.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-http_response_code(500);
-
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = (require __DIR__ . '/../config/app.php')();
