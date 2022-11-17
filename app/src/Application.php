3e481f01ba8dab62af812b1cc5e5e4823088be88
elias <eliasejump@gmail.com>
2022-11-17 13:39:07 +0300

    updates

--- app/src/Application.php
+++ app/src/Application.php
@@ -4,17 +4,27 @@ declare(strict_types=1);
 
 namespace Eliasjump\StringsVerification;
 
+use Eliasjump\StringsVerification\Controllers\MainPageController;
+use Eliasjump\StringsVerification\Controllers\StringController;
+
 final class Application
 {
     public Router $router;
 
     public function __construct()
     {
+        session_start();
+        !isset($_SESSION['counter']) ? $_SESSION['counter'] = 1 : $_SESSION['counter']++;
+
         $this->router = new Router();
     }
 
     public function run(): void
     {
+        $this->router->get('/', [MainPageController::class, 'run']);
+
+        $this->router->post('/', [StringController::class, 'run']);
+
         echo $this->router->resolve();
     }
 }
