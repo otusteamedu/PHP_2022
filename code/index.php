1a43b668891f627acbff5f3344e5238cf2d6064d
= <=>
2022-11-18 20:58:04 +0600

    HW4. Перенесла логику из клиетского кода.

--- code/index.php
+++ code/index.php
@@ -2,22 +2,13 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-use Study\StringValidator\Service\StringValidatorService;
+
 use Study\StringValidator\App;
-$se = new StringValidatorService();
-$app = new App($se);
+
+$app = new App();
+$app->run();
 
 
-if (isset($_POST['string'])){
 
-    if ($app->run($_POST['string'])){
-        echo "Строка корректа";
-        http_response_code(200);
-    } else {
-        echo "Строка некорректа.";
-        http_response_code(400);
 
-    }
-}
-
-echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
+//echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
--- code/src/App.php
+++ code/src/App.php
@@ -5,18 +5,33 @@ declare(strict_types=1);
 namespace Study\StringValidator;
 
 use Study\StringValidator\Service\StringValidatorService;
+use Study\StringValidator\Response\Response;
 
 class App
 {
     private StringValidatorService $stringValidatorService;
+    private Response $response;
 
-    public function __construct(StringValidatorService $stringValidatorService)
+    public function __construct()
     {
-       $this->stringValidatorService = $stringValidatorService;
+        $this->stringValidatorService = new StringValidatorService();
+        $this->response = new Response();
     }
 
-    public function run(string $string)
+    public function run()
     {
-        return $this->stringValidatorService->validate($string);
+        if (!isset($_POST['string'])){
+            $this->response->setStatusCode(Response::HTTP_CODE_BAD_REQUEST, "Строка не передана");
+            return $this->response->getResponse();
+        }
+        if ($this->stringValidatorService->validate($_POST['string']))
+        {
+            $this->response->setStatusCode(Response::HTTP_CODE_OK,"Строка корректа" );
+
+        } else {
+            $this->response->setStatusCode(Response::HTTP_CODE_BAD_REQUEST,"Строка некорректа" );
+
+        }
+        return $this->response->getResponse();
     }
 }
