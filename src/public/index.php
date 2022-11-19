f1ede1e877533bab7950012f156f1b9a3348337f
getinweb <79265732767@yandex.ru>
2022-11-19 19:54:55 +0300

    moved app.php & routes to web dir

--- src/public/index.php
+++ src/public/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = (require __DIR__ . '/../config/app.php')();
+$app = (require __DIR__ . '/../web/app.php')();
 
 try {
     $app->run();
