897d6550ec7823568206c508abf41646524e15f0
getinweb <79265732767@yandex.ru>
2022-11-19 18:37:42 +0300

    removed unnecessary http_response_code(500); from app.php

--- src/config/app.php
+++ src/config/app.php
@@ -6,7 +6,6 @@ use Slim\App;
 use Slim\Factory\AppFactory;
 
 return static function (): App {
-    http_response_code(500);
     $app = AppFactory::create();
     (require __DIR__ . '/routes.php')($app);
     return $app;
