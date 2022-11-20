04e3a31ec5055b9b64ad7f2ec85b6f167bb1b985
chernysh <mihail.chernysh@gmail.com>
2022-11-20 13:59:06 +0700

    Установка memcached, запуск и хранение сессии

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
+session_start();
+
 $service = new \Chernysh\Hw4\Service\CheckStringService();
 $app = new \Chernysh\Hw4\App($service);
 $app->run();
