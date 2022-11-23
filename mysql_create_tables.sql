972a437af3e76d14252c3c10e0cc78d3be1a0297
Elias <eliasejump@gmail.com>
2022-11-23 22:42:48 +0300

    update

--- mysql_create_tables.sql
+++ mysql_create_tables.sql
@@ -1,8 +1,9 @@
 CREATE TABLE films
 (
     `id`         INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    `name`       VARCHAR(255)  NOT NULL UNIQUE,
+    `title`      VARCHAR(255)  NOT NULL UNIQUE,
     `base_price` DECIMAL(8, 2) NOT NULL,
+    `deleted_at` TIMESTAMP,
     PRIMARY KEY (`id`),
 );
 
@@ -22,6 +23,7 @@ CREATE TABLE `sessions`
     `from`   TIMESTAMP     NOT NULL,
     `to`     TIMESTAMP     NOT NULL,
     `markup` DECIMAL(8, 2) NOT NULL,
+    `deleted_at` TIMESTAMP,
     PRIMARY KEY (`id`)
 );
 
@@ -29,57 +31,43 @@ CREATE TABLE `film_session`
 (
     `film_id`    INT UNSIGNED NOT NULL,
     `session_id` INT UNSIGNED NOT NULL,
-    PRIMARY KEY (`film_id`, `session_id`)
+    PRIMARY KEY (`film_id`, `session_id`),
+    CONSTRAINT `film_session_film_id_foreign`
+        FOREIGN KEY (`film_id`) REFERENCES `films` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT `film_session_session_id_foreign`
+        FOREIGN KEY (`session_id`) REFERENCES `sessions` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE `place_session`
 (
     `place_id`   INT UNSIGNED NOT NULL,
     `session_id` INT UNSIGNED NOT NULL,
-    PRIMARY KEY (`place_id`, `session_id`)
+    PRIMARY KEY (`place_id`, `session_id`),
+    CONSTRAINT `place_session_place_id_foreign`
+        FOREIGN KEY (`place_id`) REFERENCES `places` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT `place_session_session_id_foreign`
+        FOREIGN KEY (`session_id`) REFERENCES `sessions` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE `clients`
+(
+    `id`   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `orders`
 (
     `id`          INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `film_id`     INT NOT NULL,
-    `session_id`  INT NOT NULL,
-    `place_id`    INT NOT NULL,
-    `user_id`     INT NOT NULL,
-    `final_price` INT NOT NULL
+    `film_id`     INT UNSIGNED,
+    `session_id`  INT UNSIGNED,
+    `place_id`    INT UNSIGNED,
+    `user_id`     INT UNSIGNED,
+    `final_price` DECIMAL UNSIGNED NOT NULL,
+    `refund_at` TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`film_id`) REFERENCES `films` (`id`) ON UPDATE CASCADE ON DELETE SET NULL,
+    FOREIGN KEY (`session_id`) REFERENCES `sessions` (`id`) ON UPDATE CASCADE ON DELETE SET NULL,
+    FOREIGN KEY (`place_id`) REFERENCES `places` (`id`) ON UPDATE CASCADE ON DELETE SET NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `clients` (`id`) ON UPDATE CASCADE ON DELETE SET NULL
 );
-ALTER TABLE
-    `orders`
-    ADD PRIMARY KEY `orders_id_primary`(`id`);
-CREATE TABLE `clients`
-(
-    `id`   INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL
-);
-ALTER TABLE
-    `clients`
-    ADD PRIMARY KEY `clients_id_primary`(`id`);
-ALTER TABLE
-    `film_session`
-    ADD CONSTRAINT `film_session_film_id_foreign` FOREIGN KEY (`film_id`) REFERENCES `films` (`id`);
-ALTER TABLE
-    `film_session`
-    ADD CONSTRAINT `film_session_session_id_foreign` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`);
-ALTER TABLE
-    `place_session`
-    ADD CONSTRAINT `place_session_place_id_foreign` FOREIGN KEY (`place_id`) REFERENCES `places` (`id`);
-ALTER TABLE
-    `place_session`
-    ADD CONSTRAINT `place_session_session_id_foreign` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`);
-ALTER TABLE
-    `orders`
-    ADD CONSTRAINT `orders_film_id_foreign` FOREIGN KEY (`film_id`) REFERENCES `films` (`id`);
-ALTER TABLE
-    `orders`
-    ADD CONSTRAINT `orders_session_id_foreign` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`);
-ALTER TABLE
-    `orders`
-    ADD CONSTRAINT `orders_place_id_foreign` FOREIGN KEY (`place_id`) REFERENCES `places` (`id`);
-ALTER TABLE
-    `orders`
-    ADD CONSTRAINT `orders_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `clients` (`id`);
