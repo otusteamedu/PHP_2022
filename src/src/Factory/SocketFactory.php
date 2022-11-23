2b2d95722349ac9862dbcdcc84853ae284d153dd
getinweb <79265732767@yandex.ru>
2022-11-23 22:00:33 +0300

    fixed SocketFactory

--- src/src/Factory/SocketFactory.php
+++ src/src/Factory/SocketFactory.php
@@ -19,9 +19,28 @@ class SocketFactory
         if (!$socket)
             throw new Exception('Unable to create AF_UNIX socket');
 
+        self::loadSocketDir(pathinfo($socketFullPath, PATHINFO_DIRNAME));
+
+        return self::bindSocket($socket, $socketFullPath);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private static function loadSocketDir($socketDir): void
+    {
+        if (!is_dir($socketDir))
+            mkdir($socketDir, 0777, true) or
+            throw new Exception('Error by creating dir for socket file');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private static function bindSocket(Socket $socket, $socketFullPath): Socket
+    {
         if (file_exists($socketFullPath))
             unlink($socketFullPath);
-
         if (!socket_bind($socket, $socketFullPath))
             throw new Exception("Unable to bind to $socketFullPath");
 
