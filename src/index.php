79d7af5535ee8f92fec4a2753dba05e6553ec175
pingachguk <imayakimov@gmail.com>
2022-11-24 12:12:54 +0300

    добавлен балансировщик

--- src/index.php
+++ src/index.php
@@ -1,5 +1,7 @@
 <?php
 
+echo 'Сервер: '.$_SERVER['HOSTNAME'];
+
 const ERROR_DATA_MSG = 'Невалидное входящее значение';
 const ERROR_CODE = 400;
 function callError()
@@ -7,29 +9,30 @@ function callError()
     throw new Exception(ERROR_DATA_MSG, ERROR_CODE);
 }
 
-$string = $_POST['string'];
-$symbols = str_split($string);
+if (isset($_POST['string'])) {
+    $string = $_POST['string'];
+    $symbols = str_split($string);
 
-if ((strlen($string) % 2 != 0) || (strlen($string) == 0)) {
-    callError();
-}
-
-$k = 0;
-foreach ($symbols as $symbol) {
-    if ($symbol === '(') {
-        $k += 1;
-    } elseif ($symbol === ')') {
-        $k -= 1;
+    if ((strlen($string) % 2 != 0) || (strlen($string) == 0)) {
+        callError();
     }
 
-    if ($k < 0) {
+    $k = 0;
+    foreach ($symbols as $symbol) {
+        if ($symbol === '(') {
+            $k += 1;
+        } elseif ($symbol === ')') {
+            $k -= 1;
+        }
+
+        if ($k < 0) {
+            callError();
+        }
+    }
+
+    if ($k == 0) {
+        echo "ok";
+    } else {
         callError();
     }
 }
-
-if ($k == 0) {
-    echo "ok";
-} else {
-    callError();
-}
-
