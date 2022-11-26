1b8cf94492c603d14b88887ef4707899a823e417
DArtanianS <coolbase@rambler.ru>
2022-11-26 14:20:20 +0300

    fix:(sum.sh) замена bc на awk для подсчета суммы

--- sum.sh
+++ sum.sh
@@ -9,7 +9,5 @@ if ! [[ $a =~ $re && $b =~ $re ]] ; then
    echo "error: Not a number" >&2; exit 1
 fi
 
-res=`echo "$a" + "$b" | bc`
+echo  "Result: " | awk "BEGIN{print $a + $b}";
 
-echo  "Result:";
-echo  $res;
