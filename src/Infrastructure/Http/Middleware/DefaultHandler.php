9ec393cdf02ddcc02844e93f42793c1824dedeb9
andrew_marhai <buk2018irinam@gmail.com>
2022-11-27 22:10:24 +0400

    HW4: установил пакеты с интерфейсами PSR-15 , добавил реализацию миддлваров (взял отсюда https://github.com/bittyphp/middleware)

---
+++ src/Infrastructure/Http/Middleware/DefaultHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Http\Middleware;
+
+use App\Infrastructure\Http\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class DefaultHandler implements RequestHandlerInterface
+{
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return new Response('Success', 200);
+    }
+}
