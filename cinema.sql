2f0903b8c73bf30ca9d47d1fd1573cb3e68b94f8
koptev <koptev@alfa.im>
2022-11-27 09:14:20 +0300

    Added places table

--- cinema.sql
+++ cinema.sql
@@ -1,5 +1,11 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- --------------------------------------------------------
+
 CREATE TABLE `clients` (
-  `id` bigint(20) UNSIGNED ZEROFILL NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
   `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `name` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -13,7 +19,7 @@ CREATE TABLE `displays` (
   `price` double UNSIGNED DEFAULT NULL,
   `started_at` timestamp NULL DEFAULT NULL,
   `finished_at` timestamp NULL DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -21,7 +27,7 @@ CREATE TABLE `displays` (
 CREATE TABLE `halls` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -30,7 +36,17 @@ CREATE TABLE `movies` (
   `id` bigint(10) UNSIGNED NOT NULL,
   `name` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
   `country` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` int(11) NOT NULL DEFAULT current_timestamp()
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+CREATE TABLE `places` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `hall_id` int(10) UNSIGNED NOT NULL,
+  `row` int(10) UNSIGNED NOT NULL,
+  `place` int(10) UNSIGNED NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -39,35 +55,39 @@ CREATE TABLE `tickets` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `client_id` bigint(20) UNSIGNED NOT NULL,
   `display_id` bigint(20) UNSIGNED NOT NULL,
+  `place_id` bigint(20) UNSIGNED NOT NULL,
   `price` double UNSIGNED NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 ALTER TABLE `clients`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `displays`
-  ADD PRIMARY KEY (`id`),
+    ADD PRIMARY KEY (`id`),
   ADD KEY `hall_id` (`hall_id`),
   ADD KEY `movie_id` (`movie_id`);
 
 ALTER TABLE `halls`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `movies`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `places`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `hall_id` (`hall_id`);
 
 ALTER TABLE `tickets`
   ADD PRIMARY KEY (`id`),
   ADD KEY `client_id` (`client_id`),
-  ADD KEY `display_id` (`display_id`);
-
--- --------------------------------------------------------
+  ADD KEY `tickets_ibfk_2` (`display_id`),
+  ADD KEY `place_id` (`place_id`);
 
 ALTER TABLE `clients`
-  MODIFY `id` bigint(20) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT;
+    MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `displays`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
@@ -78,6 +98,9 @@ ALTER TABLE `halls`
 ALTER TABLE `movies`
   MODIFY `id` bigint(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+ALTER TABLE `places`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `tickets`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
@@ -87,6 +110,11 @@ ALTER TABLE `displays`
   ADD CONSTRAINT `displays_ibfk_1` FOREIGN KEY (`hall_id`) REFERENCES `halls` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `displays_ibfk_2` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`) ON UPDATE CASCADE;
 
+ALTER TABLE `places`
+  ADD CONSTRAINT `places_ibfk_1` FOREIGN KEY (`hall_id`) REFERENCES `halls` (`id`) ON UPDATE CASCADE;
+
 ALTER TABLE `tickets`
   ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`display_id`) REFERENCES `displays` (`id`);
+  ADD CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`display_id`) REFERENCES `displays` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `tickets_ibfk_3` FOREIGN KEY (`place_id`) REFERENCES `places` (`id`) ON UPDATE CASCADE;
+COMMIT;
