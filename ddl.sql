45462ef3032e5877f553e5194b99f8889964d253
Elias <eliasejump@gmail.com>
2022-11-29 23:26:15 +0300

    create

---
+++ ddl.sql
@@ -0,0 +1,43 @@
+CREATE TABLE films
+(
+    `id`    INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE attribute_types
+(
+    `id`    INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `field` VARCHAR(255) NOT NULL,
+    `name`  VARCHAR(255) NOT NULL UNIQUE
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE attributes
+(
+    `id`                INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `attribute_type_id` INT UNSIGNED NOT NULL,
+    `name`              VARCHAR(255) NOT NULL UNIQUE,
+    `slug`              VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `attribute_type_id_foreign`
+        FOREIGN KEY (`attribute_type_id`) REFERENCES `attribute_types` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE attribute_film
+(
+    `film_id`      INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `attribute_id` INT UNSIGNED NOT NULL,
+    `val_string`   VARCHAR(255),
+    `val_text`     TEXT,
+    `val_int`      INT,
+    `val_bool`     BOOL,
+    `val_date`     TIMESTAMP,
+    `val_float`    FLOAT,
+    PRIMARY KEY (`film_id`, `attribute_id`),
+    CONSTRAINT `attribute_film_film_id_foreign`
+        FOREIGN KEY (`film_id`) REFERENCES `films` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT `attribute_film_attribute_id_foreign`
+        FOREIGN KEY (`attribute_id`) REFERENCES `attributes` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
---
+++ dml-filling.sql
@@ -0,0 +1,13 @@
+INSERT INTO `films` (`title`) VALUES('The Green Mile');
+INSERT INTO `films` (`title`) VALUES('The Shawshank Redemption');
+INSERT INTO `films` (`title`) VALUES('Forrest Gump');
+INSERT INTO `films` (`title`) VALUES('Back to the Future');
+
+insert into attribute_types (`name`, `field`) values ('Служебная дата', 'val_date');
+insert into attribute_types (`name`, `field`) values ('Важная дата', 'val_date');
+insert into attribute_types (`name`, `field`) values ('Должности', 'val_string');
+insert into attribute_types (`name`, `field`) values ('Рецензия', 'val_text');
+insert into attribute_types (`name`, `field`) values ('Сборы', 'val_int');
+insert into attribute_types (`name`, `field`) values ('Рейтинг', 'val_float');
+insert into attribute_types (`name`, `field`) values ('Премия', 'val_bool');
+
