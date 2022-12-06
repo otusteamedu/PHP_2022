bd13eb3fccf81868a791193781a97bea99168484
m.smerdov <zdraff@gmail.com>
2022-12-06 19:06:11 +0300

    Запросы и планы готовы

--- sql/query_5.sql
+++ sql/query_5.sql
@@ -38,7 +38,27 @@ where ma.name = 'reviews';
 -- +-------------------------------------------------------------------------------------------------------------------+
 
 -- Оптимизации
+create index movie_attribute_name_index on movie_attribute (name);
+-- при текущем заполнении таблиц индекс не помог, но если будет много других movie_attribute, он пригодится
+-- как ускорить запрос в данном случае, не смог придумать
 
 
 
 -- План при 10 000 000 строк после оптимизаций
+-- +-------------------------------------------------------------------------------------------------------------------+
+-- |QUERY PLAN                                                                                                         |
+-- +-------------------------------------------------------------------------------------------------------------------+
+-- |Finalize Aggregate  (cost=110744.24..110744.25 rows=1 width=8)                                                     |
+-- |  ->  Gather  (cost=110744.02..110744.23 rows=2 width=8)                                                           |
+-- |        Workers Planned: 2                                                                                         |
+-- |        ->  Partial Aggregate  (cost=109744.02..109744.03 rows=1 width=8)                                          |
+-- |              ->  Hash Join  (cost=1.02..103494.02 rows=2500000 width=11)                                          |
+-- |                    Hash Cond: (mav.attribute_id = ma.id)                                                          |
+-- |                    ->  Parallel Seq Scan on movie_attribute_value mav  (cost=0.00..69118.00 rows=2500000 width=15)|
+-- |                    ->  Hash  (cost=1.01..1.01 rows=1 width=4)                                                     |
+-- |                          ->  Seq Scan on movie_attribute ma  (cost=0.00..1.01 rows=1 width=4)                     |
+-- |                                Filter: ((name)::text = 'reviews'::text)                                           |
+-- |JIT:                                                                                                               |
+-- |  Functions: 15                                                                                                    |
+-- |  Options: Inlining false, Optimization false, Expressions true, Deforming true                                    |
+-- +-------------------------------------------------------------------------------------------------------------------+
