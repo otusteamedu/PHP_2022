d1401351e07b10ce3e93edd682ea7e6bf38decb3
nikcrazy37 <nik_crazy@bk.ru>
2022-12-09 00:52:54 +0300

    add time table and seat table

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,33 +1,50 @@
 create table hall
 (
-    id       serial primary key,
-    capacity int not null
+    id           serial primary key,
+    seat_row     int          not null,
+    seats_in_row int          not null,
+    category     varchar(255) not null
 );
 
 create table movie
 (
     id       serial primary key,
     name     varchar(255) not null,
-    price    int          not null,
     duration time         not null
+);
+
+create table time
+(
+    id      serial primary key,
+    weekday varchar(255) not null,
+    daytime varchar(255) not null
 );
 
 create table session
 (
-    id         serial primary key,
-    start_date date not null,
-    start_time time not null,
-    movie_id   int  not null,
-    hall_id    int  not null,
+    id       serial primary key,
+    movie_id int not null,
+    hall_id  int not null,
+    time_id  int not null,
+    foreign key (movie_id) references movie (id),
     foreign key (hall_id) references hall (id),
-    foreign key (movie_id) references movie (id)
+    foreign key (time_id) references time (id)
+);
+
+create table seat
+(
+    id          serial primary key,
+    seat_row    int not null,
+    seat_number int not null
 );
 
 create table ticket
 (
-    id          serial primary key,
-    seat_number int not null,
-    session_id  int not null,
+    id         serial primary key,
+    price      int not null,
+    seat_id    int not null,
+    session_id int not null,
+    foreign key (seat_id) references seat (id),
     foreign key (session_id) references session (id)
 );
 
--- sql/select.sql
+++ sql/select.sql
@@ -1,7 +1,6 @@
-select m.name, count(m.id)*m.price as total_cost
+select m.name, sum(t.price) as total
 from ticket t
          join session s on s.id = t.session_id
          join movie m on m.id = s.movie_id
-group by m.id
-order by count(m.id)*m.price desc
-limit 1
+group by m.name
+order by total desc
