e5c332a5e97d907d3d87feda1c6cf3f1b34c2560
danil.baluev <danil.baluev@rt.ru>
2022-12-10 13:12:36 +0300

    Add ticket price and hall row and seat number

--- DDL.sql
+++ DDL.sql
@@ -1,5 +1,5 @@
 CREATE
-DATABASE cinema;
+    DATABASE cinema;
 
 CREATE TABLE hall
 (
@@ -31,13 +31,25 @@ CREATE TABLE client
     email VARCHAR(100)       NOT NULL
 );
 
+CREATE TABLE seat
+(
+    id      SERIAL PRIMARY KEY NOT NULL,
+    hall_id INTEGER            NOT NULL,
+    row     INTEGER            NOT NULL,
+    number  INTEGER            NOT NULL,
+    FOREIGN KEY (hall_id) REFERENCES hall (id)
+);
+
 CREATE TABLE ticket
 (
     id         SERIAL PRIMARY KEY NOT NULL,
     client_id  INTEGER            NOT NULL,
-    seat       INTEGER            NOT NULL,
+    seat_id    INTEGER            NOT NULL,
+    price      MONEY              NOT NULL,
     session_id SERIAL             NOT NULL,
     FOREIGN KEY (client_id) REFERENCES client (id),
+    FOREIGN KEY (seat_id) REFERENCES seat (id),
     FOREIGN KEY (session_id) REFERENCES session (id)
 );
+
 
--- query.sql
+++ query.sql
@@ -1,8 +1,8 @@
-SELECT movie.title, SUM(price) sum
+SELECT movie.title, SUM(t.price) sum
 FROM movie
-    JOIN session s
-on movie.id = s.movie_id
-    JOIN ticket t on s.id = t.session_id
+         JOIN session s
+              on movie.id = s.movie_id
+         JOIN ticket t on s.id = t.session_id
 GROUP BY movie.title
 ORDER BY sum DESC
-    LIMIT 1;
+LIMIT 1;
