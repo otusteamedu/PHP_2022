d01e9168a107ba6bdd3e0a2afc5d4f14f4c834af
nikcrazy37 <nik_crazy@bk.ru>
2022-12-10 14:51:54 +0300

    add ticket_category table

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,58 +1,57 @@
-create table hall
+CREATE TABLE HALL
 (
-    id           serial primary key,
-    seat_row     int          not null,
-    seats_in_row int          not null,
-    category     varchar(255) not null
+    ID           SERIAL PRIMARY KEY,
+    SEAT_ROW     INT          NOT NULL,
+    SEATS_IN_ROW INT          NOT NULL,
+    CATEGORY     VARCHAR(255) NOT NULL
 );
 
-create table movie
+CREATE TABLE MOVIE
 (
-    id       serial primary key,
-    name     varchar(255) not null,
-    duration time         not null
+    ID       SERIAL PRIMARY KEY,
+    NAME     VARCHAR(255) NOT NULL,
+    DURATION TIME         NOT NULL
 );
 
-create table time
+CREATE TABLE TIME
 (
-    id      serial primary key,
-    weekday varchar(255) not null,
-    daytime varchar(255) not null
+    ID      SERIAL PRIMARY KEY,
+    WEEKDAY VARCHAR(255) NOT NULL,
+    DAYTIME VARCHAR(255) NOT NULL
 );
 
-create table session
+CREATE TABLE TICKET_CATEGORY
 (
-    id       serial primary key,
-    movie_id int not null,
-    hall_id  int not null,
-    time_id  int not null,
-    foreign key (movie_id) references movie (id),
-    foreign key (hall_id) references hall (id),
-    foreign key (time_id) references time (id)
+    ID      SERIAL PRIMARY KEY,
+    PRICE   INT NOT NULL,
+    HALL_ID INT NOT NULL,
+    TIME_ID INT NOT NULL,
+    FOREIGN KEY (HALL_ID) REFERENCES HALL (ID),
+    FOREIGN KEY (TIME_ID) REFERENCES TIME (ID)
 );
 
-create table seat
+CREATE TABLE SESSION
 (
-    id          serial primary key,
-    seat_row    int not null,
-    seat_number int not null
+    ID                 SERIAL PRIMARY KEY,
+    MOVIE_ID           INT NOT NULL,
+    TICKET_CATEGORY_ID INT NOT NULL,
+    FOREIGN KEY (MOVIE_ID) REFERENCES MOVIE (ID),
+    FOREIGN KEY (TICKET_CATEGORY_ID) REFERENCES TICKET_CATEGORY (ID)
 );
 
-create table ticket
+CREATE TABLE TICKET
 (
-    id         serial primary key,
-    price      int not null,
-    seat_id    int not null,
-    session_id int not null,
-    foreign key (seat_id) references seat (id),
-    foreign key (session_id) references session (id)
+    ID         SERIAL PRIMARY KEY,
+    SEAT       INT NOT NULL,
+    SESSION_ID INT NOT NULL,
+    FOREIGN KEY (SESSION_ID) REFERENCES SESSION (ID)
 );
 
-create table viewer
+CREATE TABLE VIEWER
 (
-    id         serial primary key,
-    first_name varchar(255),
-    last_name  varchar(255),
-    ticket_id  int not null,
-    foreign key (ticket_id) references ticket (id)
+    ID         SERIAL PRIMARY KEY,
+    FIRST_NAME VARCHAR(255),
+    LAST_NAME  VARCHAR(255),
+    TICKET_ID  INT NOT NULL,
+    FOREIGN KEY (TICKET_ID) REFERENCES TICKET (ID)
 );
--- sql/select.sql
+++ sql/select.sql
@@ -1,6 +1,7 @@
-select m.name, sum(t.price) as total
+select m.name, sum(tc.price) as total
 from ticket t
-         join session s on s.id = t.session_id
-         join movie m on m.id = s.movie_id
+         join session s on t.session_id = s.id
+         join movie m on s.movie_id = m.id
+         join ticket_category tc on s.ticket_category_id = tc.id
 group by m.name
 order by total desc
