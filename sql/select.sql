566c06f669c0e457ec15af05c9015c4d48c2ed29
nikcrazy37 <nik_crazy@bk.ru>
2022-12-12 22:51:12 +0300

    remove ticket_category

--- sql/select.sql
+++ sql/select.sql
@@ -1,7 +1,6 @@
-select m.name, sum(tc.price) as total
+select m.name, sum(t.price) as total
 from ticket t
          join session s on t.session_id = s.id
          join movie m on s.movie_id = m.id
-         join ticket_category tc on s.ticket_category_id = tc.id
 group by m.name
 order by total desc
