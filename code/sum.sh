3fd6a25d5298d522601260c93ee982ca1ebcf9a0
Marina Aldoshina <aldmarinka@yandex.ru>
2022-12-19 00:35:57 +0300

    hw2 sum fix

--- code/sum.sh
+++ code/sum.sh
@@ -11,14 +11,14 @@ total=0
 
 for param in "$@"
 do
-  isnumber="$(echo $param | awk '/^[0-9-.]+$/{ print "true" }')"
-  if ! [[ $isnumber = true ]]
+  isnumber="$(echo $param | awk '/^[0-9-.]+$/{ print 1 }')"
+  if [[ $isnumber -ne 1 ]]
   then
     echo "Переданы некорректные параметры"
     exit
   fi
 
-  total=$(( $total + param ))
+  total="$(echo $total $param | awk '{ print $1 + $2 }')"
 done
 
 echo "Sum = $total"
