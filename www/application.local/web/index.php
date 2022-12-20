1af3523f24eb19f191176165946dfc06930ccdbc
m.smerdov <zdraff@gmail.com>
2022-12-20 19:11:43 +0300

    zero app

--- www/application.local/web/index.php
+++ www/application.local/web/index.php
@@ -1,51 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>application.local</title>
-</head>
-<body>
-
-Good evening, world. Today is <?=date('Y-m-d') ?>.
-
 <?php
 
-try
-{
-    $redis = new \Redis;
-    $redis->connect('redis');
-    $redis->set('rediskey', '<br>Сохранено в redis');
-    $fromRedis = $redis->get('rediskey');
-    echo $fromRedis;
-}
-catch(\Exception $e)
-{
-    echo '<br>Redis не работает: '.$e->getMessage();
-}
+use app\controllers\AppController;
 
-try {
-    $memcached = new Memcached;
-    $memcached->addServer('memcached', 11211);
-    $memcached->set('memcached_key', '<br>Сохранено в memcached');
-    echo $memcached->get('memcached_key');
+require '../vendor/autoload.php';
 
-} catch (\Exception $e) {
-    echo '<br>memcached не работает: '.$e->getMessage();
-}
-
-$connection = pg_connect ("host=db dbname=test_db user=root password=root");
-if($connection) {
-    $query = pg_query($connection, "SELECT * FROM first_table");
-    $result = pg_fetch_array($query);
-
-    echo '<br>'.$result['title'];
-} else {
-    echo '<br>there has been an error connecting';
-}
-
-phpinfo();
-?>
-
-
-</body>
-</html>
+echo (new AppController())->run();
