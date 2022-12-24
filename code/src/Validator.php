4187ae4d7e2e11f099a02a2fc327df97cfa292e8
Marina Aldoshina <aldmarinka@yandex.ru>
2022-12-25 02:27:14 +0300

    hw4 balancer

---
+++ code/src/Validator.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Maldoshina\StringValidator;
+
+class Validator
+{
+    /**
+     * @param string|null $string
+     *
+     * @return bool
+     */
+    public function validateBrackets(?string $string): bool
+    {
+        if (!$string){
+            return false;
+        }
+
+        $counter = 0;
+        foreach (str_split($string) as $symbol){
+            if ($symbol == '('){
+                $counter++;
+            }
+
+            if ($symbol == ')'){
+                $counter--;
+            }
+
+            if ($counter < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
