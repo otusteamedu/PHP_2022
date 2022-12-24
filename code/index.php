eb6370898cfff641a57e683b5491467483e7750b
Marina Aldoshina <aldmarinka@yandex.ru>
2022-12-25 02:30:07 +0300

    hw4 reformat

--- code/index.php
+++ code/index.php
@@ -1,21 +1,22 @@
 <?php
+
 declare(strict_types=1);
 
 require_once "./vendor/autoload.php";
 
 use Maldoshina\StringValidator\Validator;
 
-echo "Привет, Otus!<br>".date("Y-m-d H:i:s")."<br><br>";
+echo "Привет, Otus!<br>" . date("Y-m-d H:i:s") . "<br><br>";
 
-echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME']."<br><br>";
+echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'] . "<br><br>";
 
-$string = $_POST['string'];
+$string         = $_POST['string'];
 $validateResult = (new Validator())->validateBrackets($string);
 
 if ($validateResult) {
     http_response_code(200);
-    echo "Строка ".$string." валидна";
+    echo "Строка " . $string . " валидна";
 } else {
     http_response_code(400);
-    echo "Строка ".$string." не валидна";
+    echo "Строка " . $string . " не валидна";
 }
