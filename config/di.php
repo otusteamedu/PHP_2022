66f744c57358cc46bbb2c0ff8b0f76eac9982042
andrew_marhai <buk2018irinam@gmail.com>
2023-01-04 00:17:39 +0400

    HW10: Убрал хардкод для логина/пароля

--- config/di.php
+++ config/di.php
@@ -3,13 +3,11 @@
 // Здесь прописываем биндинг для реализаций интерфейсов
 use Elastic\Elasticsearch\ClientBuilder;
 
-// todo вынести креды в .env
 return [
     \Elastic\Elasticsearch\Client::class => function () {
         return ClientBuilder::create()
-//            ->setHosts(['https://elasticsearch:9200'])
-            ->setHosts(['https://localhost:9200'])
-            ->setBasicAuthentication('elastic', 'Gnu5PqDLyim0p0GcqfJk')
+            ->setHosts([$_ENV['ELASTIC_HOST'] . ':' . $_ENV['ELASTIC_PORT']])
+            ->setBasicAuthentication($_ENV['ELASTIC_LOGIN'], $_ENV['ELASTIC_PASSWORD'])
             ->setSSLVerification(false)
             ->build();
     }
