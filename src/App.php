c4f5a824616ab93ebd258811620f12aae7068978
AnastasiyaHahanova <anastasiya.hahanova@mail.ru>
2023-01-04 03:40:32 +0300

    hw4

---
+++ src/App.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AKhakhanova\Hw4;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class App
+{
+    private Validator $validator;
+
+    public function __construct()
+    {
+        $this->validator = new Validator();
+    }
+
+    public function run(): Response
+    {
+        if ($this->validator->isValidRequest()) {
+            return new Response('Valid request');
+        }
+
+
+        return new Response(Response::HTTP_BAD_REQUEST, 'Invalid parameter "string"');
+    }
+}
