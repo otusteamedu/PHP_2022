a8bcf8618ceba6f65cb33404d53558f7d172210f
AnastasiyaHahanova <anastasiya.hahanova@mail.ru>
2023-01-04 04:38:19 +0300

    hw4

--- public/index.php
+++ public/index.php
@@ -5,8 +5,5 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 use AKhakhanova\Hw4\App;
 
-echo "Привет, Otus!<br>" . date("Y-m-d H:i:s") . "<br><br>";
-
-echo "Homework #4";
-
-(new App())->run();
+$response = (new App())->run();
+$response->send();
