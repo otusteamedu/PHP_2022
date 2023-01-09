c2805ca9e9d4708aefdccce0785a29ddf05d43df
AnastasiyaHahanova <anastasiya.hahanova@mail.ru>
2023-01-09 10:03:03 +0300

    hw6

--- src/Server.php
+++ src/Server.php
@@ -4,37 +4,25 @@ namespace AKhakhanova\Hw4;
 
 class Server
 {
-    public function run()
+    public function run(): void
     {
-        $socket = (new UnixSocket())->create();
+        $socket = new UnixSocket();
+        if (!$socket->create(dirname(__FILE__) . "/server.sock")) {
+            echo 'An error has occurred. Try again later';
+
+            return;
+        }
         while (1) { // server never exits
-            if (!socket_set_block($socket)) {
-                die('Unable to set blocking mode for socket');
-            }
-            $buf  = '';
-            $from = '';
+            $socket->setBlock();
             echo "Ready to receive...\n";
-// will block to wait client query
-            $bytes_received = socket_recvfrom($socket, $buf, 65536, 0, $from);
-            if ($bytes_received == -1) {
-                die('An error occured while receiving from the socket');
-            }
+            [$buf, $from] = $socket->getBytesReceivedInfo();
             echo "Received $buf from $from\n";
 
             $buf .= "->Response"; // process client query here
 
-// send response
-            if (!socket_set_nonblock($socket)) {
-                die('Unable to set nonblocking mode for socket');
-            }
-// client side socket filename is known from client request: $from
-            $len        = strlen($buf);
-            $bytes_sent = socket_sendto($socket, $buf, $len, 0, $from);
-            if ($bytes_sent == -1) {
-                die('An error occured while sending to the socket');
-            } else if ($bytes_sent != $len) {
-                die($bytes_sent . ' bytes have been sent instead of the ' . $len . ' bytes expected');
-            }
+            $socket->setNonBlock();
+            $socket->sendTo($buf, $from);
+
             echo "Request processed\n";
         }
     }
--- src/UnixSocket.php
+++ src/UnixSocket.php
@@ -2,26 +2,105 @@
 
 namespace AKhakhanova\Hw4;
 
+use Monolog\Logger;
+use Socket;
+
 class UnixSocket
 {
-    public function create(): \Socket
+    private ?Socket $socket = null;
+    private Logger $logger;
+
+    public function __construct()
+    {
+        $this->logger = new Logger('socket');
+    }
+
+    public function create(string $socketFilePath): bool
     {
         if (!extension_loaded('sockets')) {
-            die('The sockets extension is not loaded.');
+            $this->logger->error('The sockets extension is not loaded.');
+
+            return false;
         }
 
-        $socket = socket_create(AF_UNIX, SOCK_DGRAM, 0);
+        $this->socket = socket_create(AF_UNIX, SOCK_DGRAM, 0);
 
-        if (!$socket) {
-            die("Unable socket");
+        if (!$this->socket) {
+            $this->logger->error('The sockets extension is not loaded.');
+
+            return false;
         }
 
-        unlink(dirname(__FILE__) . "/server.sock");
-        $server_side_sock = dirname(__FILE__) . "/server.sock";
-        if (!socket_bind($socket, $server_side_sock)) {
-            die("Unable to bind to $server_side_sock");
+        unlink($socketFilePath);
+        if (!socket_bind($this->socket, $socketFilePath)) {
+            $this->logger->error('The sockets extension is not loaded.');
+
+            return false;
         }
 
-        return $socket;
+        return true;
+    }
+
+    public function setBlock(): bool
+    {
+        if (!socket_set_block($this->socket)) {
+            $this->logger->error('Unable to set blocking mode for socket');
+        };
+
+        return true;
+    }
+
+    public function setNonBlock(): bool
+    {
+        if (!socket_set_nonblock($this->socket)) {
+            die('Unable to set nonblocking mode for socket');
+        }
+    }
+
+    public function sendTo(string $buf, string $from): int
+    {
+        $length    = strlen($buf);
+        $bytesSent = socket_sendto($this->socket, $buf, $length, 0, $from);
+
+        if ($bytesSent == -1) {
+            die('An error occured while sending to the socket');
+        } else if ($bytesSent != $length) {
+            die($bytesSent . ' bytes have been sent instead of the ' . $length . ' bytes expected');
+        }
+
+        return $bytesSent;
+    }
+
+    public function getBytesReceivedInfo(): array
+    {
+        $buf            = '';
+        $from           = '';
+        $bytes_received = socket_recvfrom($this->socket, $buf, 65536, 0, $from);
+        if ($bytes_received == -1) {
+            die('An error occurred while receiving from the socket');
+        }
+
+        return [$buf, $from, $bytes_received];
+    }
+
+    public function receiveFrom(): void
+    {
+        $buf            = '';
+        $from           = '';
+        $bytes_received = socket_recvfrom($this->socket, $buf, 65536, 0, $from);
+        if ($bytes_received == -1) {
+            die('An error occured while receiving from the socket');
+        }
+        echo "Received $buf from $from\n";
+    }
+
+    public function close(): void
+    {
+        socket_close($this->socket);
+    }
+
+    public function unlink(string $path): void
+    {
+        unlink($path);
     }
 }
