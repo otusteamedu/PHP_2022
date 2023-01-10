cee20e9f37bd00e30dde08f851f7d85f14e013d9
chernysh <mihail.chernysh@gmail.com>
2023-01-10 16:19:10 +0700

    revert // Проектирование БД

--- DDL/movies.sql
+++
@@ -1,8 +0,0 @@
-create table movies (
-    id         bigint unsigned auto_increment primary key,
-    name       varchar(255) NOT NULL,
-    duration   int          NOT NULL,
-    created_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp    NULL     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-)
-    collate = utf8mb4_unicode_ci;
--- SQL/most_popular_select.sql
+++
@@ -1,7 +0,0 @@
-select m.id, m.name, sum(hs.price) total_price
-from tickets as t
-inner join hall_shows hs on t.hall_show_id = hs.id
-inner join movies m on hs.movie_id = m.id
-group by m.id
-order by total_price desc
-limit 1;
