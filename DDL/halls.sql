806699086d476d78b4775713514fe42af1d6f4d9
chernysh <mihail.chernysh@gmail.com>
2023-01-10 16:21:08 +0700

    Проектирование БД

---
+++ DDL/halls.sql
@@ -0,0 +1,7 @@
+create table halls (
+    id         bigint unsigned auto_increment primary key,
+    name       varchar(255) NOT NULL,
+    created_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp    NULL     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+)
+    collate = utf8mb4_unicode_ci;
