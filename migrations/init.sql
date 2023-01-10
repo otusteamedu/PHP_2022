42566ef8271313a94fb86334771ae5afa5eb45f8
Dmitry Kozlov <d.kozlov@intensa.ru>
2023-01-10 22:56:05 +0300

    HW-8 Правки

--- migrations/init.sql
+++ migrations/init.sql
@@ -25,7 +25,9 @@ CREATE TABLE attribute_film
     id SERIAL PRIMARY KEY,
     attribute_id INT REFERENCES attributes(id) ON DELETE CASCADE,
     film_id INT REFERENCES films(id) ON DELETE CASCADE,
-    value TEXT
+    string_value TEXT,
+    date_value DATE,
+    bool_value BOOL
 );
 
 -- Фильмы
@@ -46,24 +48,24 @@ INSERT INTO attributes (name, type_id) VALUES ('russian_premiere', 2);
 INSERT INTO attributes (name, type_id) VALUES ('start_ticket_sale', 2);
 
 -- Заполнение атрибутов
-INSERT INTO attribute_film (attribute_id, film_id, value) VALUES (1, 1, 'Рецензия на фильм №1');
-INSERT INTO attribute_film (attribute_id, film_id, value) VALUES (1, 2, 'Рецензия на фильм №2');
-INSERT INTO attribute_film (attribute_id, film_id, value) VALUES (1, 3, 'Рецензия на фильм №3');
-INSERT INTO attribute_film (attribute_id, film_id, value) VALUES (2, 1, '1');
-INSERT INTO attribute_film (attribute_id, film_id, value) VALUES (3, 3, '2022-12-27');
-INSERT INTO attribute_film (attribute_id, film_id, value) VALUES (4, 3, '2023-01-16');
-INSERT INTO attribute_film (attribute_id, film_id, value) VALUES (5, 3, '2022-12-08');
-INSERT INTO attribute_film (attribute_id, film_id, value) VALUES (3, 1, '2022-12-27');
-INSERT INTO attribute_film (attribute_id, film_id, value) VALUES (4, 1, '2022-12-16');
-INSERT INTO attribute_film (attribute_id, film_id, value) VALUES (5, 2, '2022-01-16');
-INSERT INTO attribute_film (attribute_id, film_id, value) VALUES (3, 3, '2022-12-13');
+INSERT INTO attribute_film (attribute_id, film_id, string_value) VALUES (1, 1, 'Рецензия на фильм №1');
+INSERT INTO attribute_film (attribute_id, film_id, string_value) VALUES (1, 2, 'Рецензия на фильм №2');
+INSERT INTO attribute_film (attribute_id, film_id, string_value) VALUES (1, 3, 'Рецензия на фильм №3');
+INSERT INTO attribute_film (attribute_id, film_id, bool_value) VALUES (2, 1, '1');
+INSERT INTO attribute_film (attribute_id, film_id, date_value) VALUES (3, 3, '2023-01-10');
+INSERT INTO attribute_film (attribute_id, film_id, date_value) VALUES (4, 3, '2023-01-16');
+INSERT INTO attribute_film (attribute_id, film_id, date_value) VALUES (5, 3, '2022-12-08');
+INSERT INTO attribute_film (attribute_id, film_id, date_value) VALUES (3, 1, '2022-12-27');
+INSERT INTO attribute_film (attribute_id, film_id, date_value) VALUES (4, 1, '2022-12-16');
+INSERT INTO attribute_film (attribute_id, film_id, date_value) VALUES (5, 2, '2023-01-30');
+INSERT INTO attribute_film (attribute_id, film_id, date_value) VALUES (3, 3, '2023-01-30');
 
 -- Index
 CREATE index attribute_type ON attribute_types(type);
 
 -- View
 CREATE OR REPLACE VIEW movies_tasks AS
-    SELECT films.title, attributes.name, attribute_film.value
+    SELECT films.title, attributes.name, attribute_film.date_value as value
     FROM films
         INNER JOIN attribute_film on films.id = attribute_film.film_id
         INNER JOIN attributes on attribute_film.attribute_id = attributes.id
@@ -71,13 +73,18 @@ CREATE OR REPLACE VIEW movies_tasks AS
     WHERE
         CASE
             WHEN attribute_types.type = 'date'
-                THEN CURRENT_DATE = (attribute_film.value::DATE) OR CURRENT_DATE + 20 = (attribute_film.value::DATE)
+                THEN CURRENT_DATE = attribute_film.date_value OR CURRENT_DATE + 20 = attribute_film.date_value
             ELSE false
         END
-    ORDER BY films.title, attribute_film.value::DATE;
+    ORDER BY films.title, attribute_film.date_value;
 
 CREATE OR REPLACE VIEW movies AS
-    SELECT films.title, attribute_types.type, attributes.name, attribute_film.value
+    SELECT films.title, attribute_types.type, attributes.name,
+        CASE
+            WHEN attribute_types.type = 'string' THEN attribute_film.string_value
+            WHEN attribute_types.type = 'date' THEN attribute_film.date_value::text
+            WHEN attribute_types.type = 'bool' THEN attribute_film.bool_value::text
+        END "value"
     FROM attribute_film
         INNER JOIN films on attribute_film.film_id = films.id
         INNER JOIN attributes on attribute_film.attribute_id = attributes.id
