cb427e04e1705b79667a43d4d7dab26e8d2a7f52
AnastasiyaHahanova <anastasiya.hahanova@mail.ru>
2023-01-13 08:55:45 +0300

    hw6

--- app.php
+++ app.php
@@ -7,4 +7,3 @@ require __DIR__ . '/vendor/autoload.php';
 use AKhakhanova\Hw4\App;
 
 (new App())->run($argv);
-
--- src/App.php
+++ src/App.php
@@ -9,25 +9,20 @@ class App
         if (!in_array('server', $parameters) && !in_array('client', $parameters)) {
             print_r('Invalid input parameters. Please choose one of the options "client" or "server"');
         }
-
         if (in_array('server', $parameters) && in_array('client', $parameters)) {
             print_r('Please choose only one of the options: "client" or "server"');
         }
-
         try {
             if (in_array('client', $parameters)) {
                 (new Client())->run();
             }
-
             if (in_array('server', $parameters)) {
                 (new Server())->run();
             }
+        } catch (\Throwable $exception) {
+            print_r('An error has occurred. ' . $exception->getMessage());
 
-        }catch (\Throwable $exception)
-        {
-            print_r('An error has occurred. '. $exception->getMessage());
             return;
         }
-
     }
 }
