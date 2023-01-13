ba3dd54923251a19198f70f86c6efa7e3bc06cc5
Igor Tsyganok <ciganokigor@gmail.com>
2023-01-13 16:10:19 +0500

    Изменил название метода и работу с массивом
    Убрал усложнение... не надо было кодить уставшим..)

--- code/src/Application/Entity/Producer/BankSender.php
+++ code/src/Application/Entity/Producer/BankSender.php
@@ -2,12 +2,11 @@
 
 namespace Otus\App\Application\Entity\Producer;
 
-use Otus\App\Application\Entity\Configurator;
+use Otus\App\Application\Entity\ConfiguratorRabbitMQ;
 use Otus\App\Application\Entity\Messenger\MailMessenger;
 use Otus\App\Application\Services\MessageForSend;
 use Otus\App\Domain\Models\Interface\SendInterface;
 use PhpAmqpLib\Message\AMQPMessage;
-
 
 class BankSender implements SendInterface
 {
@@ -15,12 +14,11 @@ class BankSender implements SendInterface
 
     public function __construct()
     {
-        $connection = Configurator::createdChannel();
-        $array_for_message = BankSender::sendMessage();
-        $email = current($array_for_message);
-        $array_date = array_slice($array_for_message, 1, 2);
-        $data_start = current($array_date);
-        $data_end = end($array_date);;
+        $connection = ConfiguratorRabbitMQ::createdChannel();
+        $array_for_message = BankSender::generateMessage();
+        $email = $array_for_message[0];
+        $data_start = $array_for_message[1];
+        $data_end = $array_for_message[2];
         $bank_message = "Отправим на $email выписку за период $data_start - $data_end ";
         $msg = new AMQPMessage($bank_message);
         $connection->basic_publish($msg, '', 'message_from_bank');
@@ -28,12 +26,10 @@ class BankSender implements SendInterface
         $email_message->send($email);
     }
 
-    public static function sendMessage()
+    public static function generateMessage()
     {
         $created_message = new MessageForSend();
         $bank_message = $created_message->create();
         return $bank_message;
     }
-
-
 }
