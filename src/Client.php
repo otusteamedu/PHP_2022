b73293d0ea60c5faad13ab362369dbc89111f23b
AnastasiyaHahanova <anastasiya.hahanova@mail.ru>
2023-01-14 14:42:17 +0300

    hw6

--- src/Client.php
+++ src/Client.php
@@ -9,20 +9,44 @@ class Client
     public function run(): void
     {
         try {
-            $socket         = new UnixSocket();
-            $socketFilePath = dirname(__FILE__) . "/client.sock";
+            $socket               = new UnixSocket();
+            $socketFilePath       = dirname(__FILE__) . "/client.sock";
             $serverSocketFilePath = dirname(__FILE__) . "/server.sock";
             $socket->create($socketFilePath);
             $socket->setNonBlock();
-            $msg              = "Message";
-            $socket->sendTo($msg, $serverSocketFilePath);
-            $socket->setBlock();
-            [$buf, $from] = $socket->receive();
-            print_r(sprintf('Message "%s" was successfully sent', $buf));
+
+            $stdin = fopen('php://stdin', 'r');
+            $exit  = 0;
+            print_r('Enter the message: ');
+            while (1) {
+                $msg = trim(fgets($stdin));
+                if (empty($msg) && $exit) {
+                    break;
+                }
+
+                if (empty($msg)) {
+                    $exit++;
+                    print_r('Please type the message or press "Enter" to exit.');
+                    continue;
+                }
+
+                if ($exit) {
+                    $exit--;
+                }
+
+                $socket->sendTo($msg, $serverSocketFilePath);
+                $socket->setBlock();
+                [$buf, $from] = $socket->receive();
+                print_r(sprintf('Message "%s" was successfully sent.', trim($buf)) . PHP_EOL . PHP_EOL);
+                print_r('Enter the message: ');
+            }
+
+            fclose($stdin);
             $socket->close();
             $socket->unlink($socketFilePath);
         } catch (Throwable $e) {
             print_r('An error has occurred. ' . $e->getMessage());
+
             return;
         }
 
--- src/Server.php
+++ src/Server.php
@@ -10,12 +10,12 @@ class Server
         $socket->create(dirname(__FILE__) . "/server.sock");
         while (1) {
             $socket->setBlock();
-            print_r("Ready to receive...");
+            print_r("Ready to receive..." . PHP_EOL);
             [$buf, $from] = $socket->receive();
-            print_r("Received \"$buf\" from $from");
+            print_r(sprintf('Received "%s" from "%s"', trim($buf), trim($from)) . PHP_EOL);
             $socket->setNonBlock();
             $socket->sendTo($buf, $from);
-            print_r("Request processed");
+            print_r("Request processed" . PHP_EOL.PHP_EOL);
         }
     }
 }
