2042e64f719c8605bd48fb59625de2467d46239a
sveta <svatelnet@gmail.com>
2023-01-17 21:27:54 +0300

    Правки

--- code/src/Client.php
+++ code/src/Client.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Sveta\Code;
 
-use Sveta\Code\UnixSocket;
 use Throwable;
 
 final class Client
@@ -12,41 +11,12 @@ final class Client
     public function run(): void
     {
         try {
-            $socket               = new UnixSocket();
             $socketFilePath       = dirname(__FILE__) . "/socket/client.sock";
             $serverSocketFilePath = dirname(__FILE__) . "/socket/server.sock";
-            $socket->create($socketFilePath);
-            $socket->setNonBlock();
 
-            $stdin = fopen('php://stdin', 'r');
-            $exit  = 0;
-            print_r('Enter the message: ');
-            while (1) {
-                $msg = trim(fgets($stdin));
-                if (empty($msg) && $exit) {
-                    break;
-                }
+            $socket = $this->createUnixSocket($socketFilePath);
 
-                if (empty($msg)) {
-                    $exit++;
-                    print_r('Please type the message or press "Enter" to exit.');
-                    continue;
-                }
-
-                if ($exit) {
-                    $exit--;
-                }
-
-                $socket->sendTo($msg, $serverSocketFilePath);
-                $socket->setBlock();
-                [$buf, $from] = $socket->receive();
-                print_r(sprintf('Message "%s" was successfully sent.', trim($buf)) . PHP_EOL . PHP_EOL);
-                print_r('Enter the message: ');
-            }
-
-            fclose($stdin);
-            $socket->close();
-            $socket->unlink($socketFilePath);
+            $this->send($socket, $socketFilePath, $serverSocketFilePath);
         } catch (Throwable $e) {
             print_r('An error has occurred. ' . $e->getMessage());
 
@@ -55,4 +25,53 @@ final class Client
 
         echo "Client exits\n";
     }
+
+    /**
+     * @throws \Exception
+     */
+    public function send(UnixSocket $socket, string $socketFilePath, string $serverSocketFilePath): void
+    {
+        $stdin = fopen('php://stdin', 'r');
+        $exit  = 0;
+        print_r('Enter the message: ');
+        while (1) {
+            $msg = trim(fgets($stdin));
+            if (empty($msg) && $exit) {
+                break;
+            }
+
+            if (empty($msg)) {
+                $exit++;
+                print_r('Please type the message or press "Enter" to exit.');
+                continue;
+            }
+
+            if ($exit) {
+                $exit--;
+            }
+
+            $socket->sendTo($msg, $serverSocketFilePath);
+            $socket->setBlock();
+            [$buf, $from] = $socket->receive();
+            print_r(sprintf('Message "%s" was successfully sent.', trim($buf)) . PHP_EOL . PHP_EOL);
+            print_r('Enter the message: ');
+        }
+
+        fclose($stdin);
+        $socket->close();
+        $socket->unlink($socketFilePath);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function createUnixSocket(string $socketFilePath): UnixSocket
+    {
+        $socket = new UnixSocket();
+        $socket->create($socketFilePath);
+        $socket->setNonBlock();
+
+        return $socket;
+    }
 }
+
--- code/src/Server.php
+++ code/src/Server.php
@@ -4,14 +4,35 @@ declare(strict_types=1);
 
 namespace Sveta\Code;
 
-use Sveta\Code\UnixSocket;
+use Throwable;
 
 final class Server
 {
     public function run(): void
     {
+        try {
+            $socket = $this->createUnixSocket();
+            $this->send($socket);
+        } catch (Throwable $e) {
+            print_r('An error has occurred. ' . $e->getMessage());
+
+            return;
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function createUnixSocket(): UnixSocket
+    {
         $socket = new UnixSocket();
         $socket->create(dirname(__FILE__) . "/socket/server.sock");
+
+        return $socket;
+    }
+
+    public function send(UnixSocket $socket): void
+    {
         while (1) {
             $socket->setBlock();
             print_r("Ready to receive..." . PHP_EOL);
@@ -23,3 +44,4 @@ final class Server
         }
     }
 }
+
