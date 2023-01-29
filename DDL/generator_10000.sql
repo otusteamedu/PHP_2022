a4a444066d7994140cb07bff745e06eac5f85304
chernysh <mihail.chernysh@gmail.com>
2023-01-29 19:12:50 +0700

    Таблички с кинозалами + генератор. Продажа билетиков.

--- DDL/generator_10000.sql
+++ DDL/generator_10000.sql
@@ -10,3 +10,40 @@ select
     (random() < 0.1)::bool
 from
     generate_series(1, 10000) as gs(id);
+
+
+
+-- 1440
+insert into halls_places (hall_id, line, number)
+select halls.id, gs1.line, gs2.number
+from
+    halls,
+    generate_series(1, 15) as gs1(line),
+    generate_series(1, 32) as gs2(number);
+
+
+
+insert into movies_shows (movie_id, hall_id, price, start_date)
+select
+    m.id,
+    (1+random()*2)::integer,
+        10*(35+random()*10)::integer,
+        timestamp '2022-12-01' + random() * (timestamp '2023-02-28' - timestamp '2022-12-01')
+from movies as m;
+
+-- рандомно продаем билетики
+WITH vars AS (
+    SELECT
+        (select count(*) from halls_places) as countPlaces,
+        (select min(id) from halls_places) as minPlaces,
+        (select count(*) from movies_shows) as countShows,
+        (select min(id) from movies_shows) as minShows
+)
+insert into tickets (id, show_id, place_id)
+select
+    gs.id,
+    (vars.minShows+random()*(vars.countShows-1))::integer as show_id,
+    (vars.minPlaces+random()*(vars.countPlaces-1))::integer as place_id
+from
+    vars,
+    generate_series(1, 100000) as gs(id);
--- DDL/tables.sql
+++ DDL/tables.sql
@@ -1,7 +1,7 @@
 DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 
--- Таблица с кинофильмами
+-- Кинофильмы
 CREATE TABLE movies (
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(50),
@@ -9,4 +9,53 @@ CREATE TABLE movies (
     duration    INT       NOT NULL,
     is_deleted  BOOL      NOT NULL DEFAULT false,
     created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- Залы
+CREATE TABLE halls (
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(50),
+    is_deleted  BOOL      NOT NULL DEFAULT false,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO halls (id, name) VALUES (1, 'Синий зал');
+INSERT INTO halls (id, name) VALUES (2, 'Красный зал');
+INSERT INTO halls (id, name) VALUES (3, 'Оранжевый зал');
+
+-- Места в зале
+CREATE TABLE halls_places (
+    id         SERIAL PRIMARY KEY,
+    hall_id    INT       NOT NULL,
+    line       INT       NOT NULL,
+    number     INT       NOT NULL,
+    is_deleted BOOL      NOT NULL DEFAULT false,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (hall_id) REFERENCES halls (id)
+);
+CREATE UNIQUE INDEX idx_halls_places_line_number ON halls_places (hall_id, line, number);
+
+-- Показы фильмов
+create table movies_shows (
+    id         SERIAL PRIMARY KEY,
+    hall_id    INT       NOT NULL,
+    movie_id   INT       NOT NULL,
+    price      INT       NOT NULL,
+    start_date timestamp NOT NULL,
+    is_deleted BOOL      NOT NULL DEFAULT false,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (hall_id) REFERENCES halls (id),
+    FOREIGN KEY (movie_id) REFERENCES movies (id)
+);
+
+-- Билетики
+create table tickets (
+    id         SERIAL PRIMARY KEY,
+    show_id    INT       NOT NULL,
+    place_id   INT       NOT NULL,
+    is_deleted BOOL      NOT NULL DEFAULT false,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (show_id) REFERENCES movies_shows (id),
+    FOREIGN KEY (place_id) REFERENCES halls_places (id)
 );
