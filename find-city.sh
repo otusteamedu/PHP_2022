46db1190a1d349a50eb41fae106f3183b359fd53
Mikhail Afonin <mafonin@sweb.ru>
2023-03-05 22:28:29 +0300

    Homework 2

--- find-city.sh
+++ find-city.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # This is script for find most popular city in file table.txt
 
-while read str
-do
-echo "Line contents are : $str "
-done < table.txt
+awk '{print $3}' table.txt | sort -k1 | uniq -c | sort -rn | head -n 3
