a1bfa7d3cea4bc464a56056faeea710a8e2eca32
Andrew <buk2018irinam@gmail.com>
2023-03-12 18:41:50 +0400

    HW17: добавил использование очереди при обработке дооолгих процессов

--- src/Infrastructure/Http/Controller/Process/CreateProcessController.php
+++ src/Infrastructure/Http/Controller/Process/CreateProcessController.php
@@ -6,6 +6,7 @@ namespace App\Infrastructure\Http\Controller\Process;
 
 use App\Application\Queue\BusInterface;
 use App\Domain\Enum\ProcessStatus;
+use App\Infrastructure\Queue\Process\OperateProcessMessage;
 use Bitty\Http\JsonResponse;
 use Psr\Http\Message\ServerRequestInterface;
 use App\Application\UseCase\Process\ProcessCreator;
@@ -15,13 +16,18 @@ use App\Application\UseCase\Process\ProcessCreator;
  */
 class CreateProcessController
 {
-    public function __construct(private readonly ProcessCreator $processCreator)
+    public function __construct(
+        private readonly ProcessCreator $processCreator,
+        private readonly BusInterface $bus
+    )
     {
     }
 
     public function __invoke(ServerRequestInterface $request): JsonResponse
     {
         $process = $this->processCreator->create();
+
+        $this->bus->dispatch(new OperateProcessMessage($process));
 
         return new JsonResponse([
             'processId' => $process->getId(),
