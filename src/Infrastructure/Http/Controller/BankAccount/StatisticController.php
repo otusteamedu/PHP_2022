ed5bfd5796423f852d7f1b4f6642fbb77875ae44
Andrew <buk2018irinam@gmail.com>
2023-03-12 11:58:33 +0400

    HW16: Добавил контроллер с отправкой сообщения в очередь

--- src/Infrastructure/Http/Controller/BankAccount/StatisticController.php
+++ src/Infrastructure/Http/Controller/BankAccount/StatisticController.php
@@ -1,16 +1,29 @@
 <?php
 
-namespace App\Application\Controller\BankAccount;
+namespace App\Infrastructure\Http\Controller\BankAccount;
 
-use Bitty\Http\Response;
+use App\Application\Queue\BusInterface;
+use App\Infrastructure\Queue\BankAccount\GenerateStatisticByDatesMessage;
+use Bitty\Http\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class StatisticController
 {
+    public function __construct(private readonly BusInterface $bus)
+    {
+    }
+
     public function generate(ServerRequestInterface $request): ResponseInterface
     {
-        dd($request);
-        return new Response('Hello, world!');
+        $this->bus->dispatch(new GenerateStatisticByDatesMessage(
+            new \DateTimeImmutable($request->getParsedBody()['date-start']),
+            new \DateTimeImmutable($request->getParsedBody()['date-end']),
+        ));
+
+        return new JsonResponse([
+            'status' => 'processing',
+            'msg' => 'We will notify you when the report is ready'
+        ]);
     }
 }
