528566317fb32c718a013bdd1435e2c1cf6a2892
Aleksej Fominichev <rehkzylbz@yandex.com>
2023-05-30 01:36:17 +0300

    seat types added

--- DDL.sql
+++ DDL.sql
@@ -17,7 +17,8 @@ CREATE TABLE "seats" (
   "id" integer PRIMARY KEY,
   "hall_id" integer REFERENCES "halls" ("id"),
   "row" integer,
-  "number" integer
+  "number" integer,
+  "seat_type_id" integer REFERENCES "seat_types" ("id")  
 );
 
 CREATE TABLE "shows" (
@@ -38,7 +39,12 @@ CREATE TABLE "tickets" (
 
 CREATE TABLE "prices" (
   "id" integer PRIMARY KEY,
-  "seat_id" integer REFERENCES "seats" ("id"),
+  "seat_type_id" integer REFERENCES "seat_types" ("id"),
   "price" integer REFERENCES "shows" ("id"),
   "show_id" integer
+);
+
+CREATE TABLE "seat_types" (
+  "id" integer PRIMARY KEY,
+  "type_name" text
 );
