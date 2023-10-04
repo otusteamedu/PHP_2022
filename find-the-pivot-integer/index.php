7ba76bb90061f6c6589ec7b4ab799f425850d848
getinweb <79265732767@yandex.ru>
2023-10-04 19:57:58 +0300

    added  solutions

---
+++ find-the-pivot-integer/index.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ *  Сложность линейная O(n)
+ *
+ *  по памяти константная O(1)
+ *
+ *  https://leetcode.com/problems/find-the-pivot-integer/submissions/1065225100/
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function pivotInteger($n)
+    {
+        if ($n === 1) {
+            return 1;
+        }
+
+        for ($i = 2; $i < $n; $i++) {
+            if ($this->arithmeticProgressionSum(1, $i - 1) === $this->arithmeticProgressionSum($i + 1, $n)) {
+                return $i;
+            }
+        }
+
+        return -1;
+    }
+
+    private function arithmeticProgressionSum(int $n, int $m): int
+    {
+        return 0.5 * ($m + $n) * ($m - $n + 1);
+    }
+
+}
---
+++ product-of-array-except-self/index.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ *  Сложность линейная O(n + n-1 + n-1) = O(n)
+ *   три цикла по n, n-1, n-1       ~3n, константу отбрасываем
+ *
+ *  по памяти также O(n)
+ *
+ *  https://leetcode.com/problems/product-of-array-except-self/submissions/1065161879/
+ */
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function productExceptSelf($nums)
+    {
+        $answer = [];
+
+        $leftProds = $this->leftProds($nums);
+        $rightProds = $this->rightProds($nums);
+
+        foreach ($nums as $key => $num) {
+            $answer[] = $leftProds[$key] * $rightProds[$key];
+        }
+
+        return $answer;
+    }
+
+    private function leftProds(array $nums): array
+    {
+        $result = [];
+        $length = count($nums);
+        $result[] = 1;
+
+        for ($i = 1; $i < $length; $i++) {
+            $result[$i] = $result[$i - 1] * $nums[$i - 1];
+        }
+
+        return $result;
+    }
+
+    private function rightProds(array $nums): array
+    {
+        $result = [];
+        $length = count($nums);
+        $result[$length - 1] = 1;
+
+        for ($i = $length - 2; $i >= 0; $i--) {
+            $result[$i] = $result[$i + 1] * $nums[$i + 1];
+        }
+
+        return $result;
+    }
+}
